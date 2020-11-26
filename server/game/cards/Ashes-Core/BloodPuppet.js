ce0ddf3ebc8b1bc86946e072f75d161c55b89db5
Dan Johnson <danj3000@outlook.com>
2020-11-26 11:55:53 +0000

    blood puppet and minor jsx error fix

---
+++ server/game/cards/Ashes-Core/BloodPuppet.js
@@ -0,0 +1,35 @@
+const { Level } = require('../../../constants.js');
+const Card = require('../../Card.js');
+const DiceCount = require('../../DiceCount.js');
+
+class BloodPuppet extends Card {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            location: 'any',
+            targetController: 'any',
+            effect: ability.effects.entersPlayUnderOpponentsControl()
+        });
+
+        // end of round wounds pb
+        this.forcedInterrupt({
+            title: 'Cursed 1',
+            when: {
+                onRoundEnded: () => true
+            },
+            gameAction: ability.actions.dealDamage((context) => ({
+                target: context.source.owner.phoenixborn
+            }))
+        });
+
+        // side action
+        this.action({
+            title: 'Self Inflict',
+            cost: [ability.costs.sideAction(), ability.costs.dice([new DiceCount(1, Level.Basic)])],
+            gameAction: ability.actions.dealDamage({ target: this })
+        });
+    }
+}
+
+BloodPuppet.id = 'blood-puppet';
+
+module.exports = BloodPuppet;
