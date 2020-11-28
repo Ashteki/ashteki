9fc2597c60dfd906420328c6fb7eb145141ae991
Dan Johnson <danj3000@outlook.com>
2020-11-28 06:54:15 +0000

    fade away using conditionalAction

--- server/game/cards/Ashes-Core/FadeAway.js
+++ server/game/cards/Ashes-Core/FadeAway.js
@@ -1,14 +1,22 @@
+const { CardType } = require('../../../constants.js');
 const Card = require('../../Card.js');
 
 class FadeAway extends Card {
     setupCardAbilities(ability) {
-        this.interrupt({
+        this.forcedReaction({
             title: 'Fade Away',
             when: {
                 onRoundEnded: () => true
             },
-            gameAction: ability.actions.destroy((context) => ({
-                card: context.source
+            gameAction: ability.actions.conditional((context) => ({
+                target: context.source.parent,
+                condition: () => context.source.parent.type === CardType.Ally,
+                trueGameAction: ability.actions.purge({
+                    target: context.source.parent
+                }),
+                falseGameAction: ability.actions.destroy({
+                    target: context.source.parent
+                })
             }))
         });
     }
