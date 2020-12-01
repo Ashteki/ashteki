d8e6cc69e5ee4ed5865f2b7f4c55fdca555b6a76
Dan Johnson <danj3000@outlook.com>
2020-12-01 21:24:35 +0000

    fix #53 - gilder exit message

---
+++ test/server/cards/Ashes-Core/Gilder.spec.js
@@ -0,0 +1,31 @@
+describe('Gilder', function () {
+    beforeEach(function () {
+        this.setupTest({
+            player1: {
+                phoenixborn: 'aradel-summergaard',
+                inPlay: ['blood-shaman'],
+                dicepool: ['charm', 'natural', 'natural', 'illusion', 'charm', 'charm'],
+                hand: ['molten-gold']
+            },
+            player2: {
+                phoenixborn: 'maeoni-viper',
+                inPlay: ['gilder', 'iron-worker'],
+                hand: ['out-of-the-mist'],
+                dicepool: ['charm', 'natural', 'natural', 'illusion', 'charm', 'charm']
+            }
+        });
+
+        this.aradelSummergaard.tokens.damage = 1;
+        this.player1.dicepool[0].lower();
+    });
+
+    it('adds a status token on destroyed', function () {
+        this.player1.clickCard(this.moltenGold);
+        this.player1.clickPrompt('Play this action');
+        this.player1.clickCard(this.gilder);
+
+        this.player2.clickCard(this.ironWorker);
+
+        expect(this.ironWorker.status).toBe(1);
+    });
+});
