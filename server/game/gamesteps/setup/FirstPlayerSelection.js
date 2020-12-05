1e35bc59c84e3ddf42ef30d068f297766b3196e6
Dan Johnson <danj3000@outlook.com>
2020-12-05 11:33:38 +0000

    first player is a choice fixes #10

--- server/game/gamesteps/setup/FirstPlayerSelection.js
+++ server/game/gamesteps/setup/FirstPlayerSelection.js
@@ -1,30 +1,24 @@
-const AllPlayerPrompt = require('../allplayerprompt');
-const _ = require('underscore');
+const UiPrompt = require('../uiprompt');
 
-class FirstPlayerSelection extends AllPlayerPrompt {
+class FirstPlayerSelection extends UiPrompt {
     constructor(game) {
         super(game);
         this.previousWinner = game.previousWinner;
         this.clickedButton = false;
         this.players = game.getPlayers();
-    }
-
-    completionCondition(player) {
-        return this.previousWinner === player.name || !this.previousWinner || this.clickedButton;
+        this.firstPlayer = null;
     }
 
     activePrompt() {
         return {
             promptTitle: 'First Player',
             menuTitle: 'Who will go first?',
-            buttons: this.players
-                .map((player) => ({ arg: player.name, text: player.name }))
-                .concat({ arg: 'random', text: 'random' })
+            buttons: this.players.map((player) => ({ arg: player.name, text: player.name }))
         };
     }
 
     waitingPrompt() {
-        return { menuTitle: 'Waiting for opponent to choose to continue who will go first' };
+        return { menuTitle: 'Waiting for opponent to choose first player' };
     }
 
     menuCommand(player, arg) {
@@ -32,31 +26,27 @@ class FirstPlayerSelection extends AllPlayerPrompt {
         let message;
 
         if (!otherPlayer) {
-            this.game.activePlayer = player;
+            this.firstPlayer = player;
             message = '{0} will go first';
         } else if (arg === player.name) {
-            this.game.activePlayer = player;
+            this.firstPlayer = player;
             message = '{0} chooses to go first';
         } else if (arg === otherPlayer.name) {
-            this.game.activePlayer = otherPlayer;
+            this.firstPlayer = otherPlayer;
             message = '{0} chooses to go second';
-        } else {
-            message = '{0} chooses to randomize the first player';
         }
 
         this.game.addMessage(message, player.name);
-
         this.clickedButton = true;
+
+        this.complete();
 
         return true;
     }
 
     onCompleted() {
-        if (!this.game.activePlayer) {
-            let allPlayersShuffled = _.shuffle(this.game.getPlayers());
-            this.game.activePlayer = allPlayersShuffled.shift();
-            this.game.addMessage('{0} won the flip and is first player', this.game.activePlayer);
-        }
+        this.game.firstPlayer = this.firstPlayer;
+        this.game.firstPlayer.setFirstPlayer();
     }
 }
 
