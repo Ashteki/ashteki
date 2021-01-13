ec6f33460cef05e85692d4c4e8a75ac370e850f3
Dan Johnson <danj3000@outlook.com>
2021-01-13 13:45:29 +0000

    massive growth test

---
+++ test/server/cards/Ashes-Core/MassiveGrowth.spec.js
@@ -0,0 +1,46 @@
+describe('Massive Growth alteration', function () {
+    beforeEach(function () {
+        this.setupTest({
+            player1: {
+                phoenixborn: 'rin-northfell',
+                inPlay: ['mist-spirit'],
+                dicepool: ['natural', 'natural', 'natural', 'charm'],
+                spellboard: [],
+                hand: ['massive-growth']
+            },
+            player2: {
+                phoenixborn: 'coal-roarkwin',
+                inPlay: ['iron-worker']
+            }
+        });
+    });
+
+    it('attaches to target card, grants attack and life', function () {
+        this.player1.clickCard(this.massiveGrowth);
+        this.player1.clickPrompt('Play this Alteration');
+        this.player1.clickDie(2);
+        this.player1.clickPrompt('Done');
+        this.player1.clickCard(this.mistSpirit);
+        expect(this.mistSpirit.upgrades.length).toBe(1);
+
+        expect(this.mistSpirit.attack).toBe(5);
+        expect(this.mistSpirit.life).toBe(5);
+
+        this.player1.endTurn();
+        this.player2.clickPrompt('Attack');
+        this.player2.clickCard(this.mistSpirit);
+        this.player2.clickCard(this.ironWorker);
+        this.player1.clickPrompt('Done');
+        this.player1.clickPrompt('No'); // don't counter
+
+        expect(this.mistSpirit.damage).toBe(2);
+        expect(this.mistSpirit.location).toBe('play area');
+
+        this.player2.endTurn();
+        this.player1.clickPrompt('Attack');
+        this.player1.clickCard(this.coalRoarkwin);
+        this.player1.clickCard(this.mistSpirit);
+        this.player1.clickPrompt('Done');
+        expect(this.coalRoarkwin.damage).toBe(5);
+    });
+});
