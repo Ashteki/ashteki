2ef4252dfa0ae518a502bc0896601ea925dc7cc9
Dan Johnson <danj3000@outlook.com>
2021-01-15 11:17:23 +0000

    ambush / play ability fix to remove incorrect onfight trigger fix #115

--- test/server/cards/Ashes-Core/StormwindSniper.spec.js
+++ test/server/cards/Ashes-Core/StormwindSniper.spec.js
@@ -1,60 +1,93 @@
 describe('Stormwind Sniper', function () {
-    beforeEach(function () {
-        this.setupTest({
-            player1: {
-                phoenixborn: 'aradel-summergaard',
-                inPlay: ['mist-spirit'],
-                dicepool: ['natural', 'natural', 'illusion', 'charm', 'charm'],
-                spellboard: ['summon-butterfly-monk'],
-                hand: ['molten-gold']
-            },
-            player2: {
-                phoenixborn: 'maeoni-viper',
-                inPlay: ['stormwind-sniper', 'iron-worker'],
-                spellboard: []
-            }
+    describe('Concealed', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['mist-spirit'],
+                    dicepool: ['natural', 'natural', 'illusion', 'charm', 'charm'],
+                    spellboard: ['summon-butterfly-monk'],
+                    hand: ['molten-gold']
+                },
+                player2: {
+                    phoenixborn: 'maeoni-viper',
+                    inPlay: ['stormwind-sniper', 'iron-worker'],
+                    spellboard: []
+                }
+            });
+        });
+
+        it('is concealed from spell targetting', function () {
+            this.player1.clickCard(this.moltenGold); // play seal
+            this.player1.clickPrompt('Play this action');
+            expect(this.player1).toHavePrompt('Choose a card');
+            // concealed!
+            expect(this.player1).toBeAbleToSelect(this.ironWorker);
+            expect(this.player1).not.toBeAbleToSelect(this.stormwindSniper);
+        });
+
+        it('is not concealed from spell targetting when exhausted', function () {
+            exhaustCard(this.stormwindSniper, this.game);
+
+            this.player1.clickCard(this.moltenGold); // play seal
+            this.player1.clickPrompt('Play this action');
+            expect(this.player1).toHavePrompt('Choose a card');
+            // concealed!
+            expect(this.player1).toBeAbleToSelect(this.ironWorker);
+            expect(this.player1).toBeAbleToSelect(this.stormwindSniper);
+        });
+
+        it('is concealed from unit attacks', function () {
+            this.player1.clickPrompt('Attack');
+            expect(this.player1).toHavePrompt('Select a target to attack');
+            // concealed!
+            expect(this.player1).toBeAbleToSelect(this.ironWorker);
+            expect(this.player1).not.toBeAbleToSelect(this.stormwindSniper);
+        });
+
+        it('is not concealed from unit attacks when exhausted', function () {
+            exhaustCard(this.stormwindSniper, this.game);
+
+            this.player1.clickPrompt('Attack');
+            expect(this.player1).toHavePrompt('Select a target to attack');
+            // concealed!
+            expect(this.player1).toBeAbleToSelect(this.ironWorker);
+            expect(this.player1).toBeAbleToSelect(this.stormwindSniper);
+        });
+
+        function exhaustCard(card, game) {
+            game.actions.exhaust().resolve(card, game.getFrameworkContext());
+        }
+    });
+
+    describe('Attacking', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['stormwind-sniper'],
+                    dicepool: ['natural', 'natural', 'illusion', 'charm', 'charm'],
+                    spellboard: ['summon-butterfly-monk'],
+                    hand: ['molten-gold']
+                },
+                player2: {
+                    phoenixborn: 'maeoni-viper',
+                    inPlay: ['hammer-knight', 'iron-worker'],
+                    spellboard: []
+                }
+            });
+        });
+
+        it('BUG: sniper attack value is 2, no pb damage', function () {
+            this.player1.clickPrompt('Attack');
+            this.player1.clickCard(this.hammerKnight);
+            this.player1.clickCard(this.stormwindSniper);
+            this.player2.clickPrompt('Done'); // guard
+            this.player2.clickPrompt('No'); // counter
+
+            // bug check for damage
+            expect(this.hammerKnight.damage).toBe(2);
+            expect(this.maeoniViper.damage).toBe(0);
         });
     });
-
-    it('is concealed from spell targetting', function () {
-        this.player1.clickCard(this.moltenGold); // play seal
-        this.player1.clickPrompt('Play this action');
-        expect(this.player1).toHavePrompt('Choose a card');
-        // concealed!
-        expect(this.player1).toBeAbleToSelect(this.ironWorker);
-        expect(this.player1).not.toBeAbleToSelect(this.stormwindSniper);
-    });
-
-    it('is not concealed from spell targetting when exhausted', function () {
-        exhaustCard(this.stormwindSniper, this.game);
-
-        this.player1.clickCard(this.moltenGold); // play seal
-        this.player1.clickPrompt('Play this action');
-        expect(this.player1).toHavePrompt('Choose a card');
-        // concealed!
-        expect(this.player1).toBeAbleToSelect(this.ironWorker);
-        expect(this.player1).toBeAbleToSelect(this.stormwindSniper);
-    });
-
-    it('is concealed from unit attacks', function () {
-        this.player1.clickPrompt('Attack');
-        expect(this.player1).toHavePrompt('Select a target to attack');
-        // concealed!
-        expect(this.player1).toBeAbleToSelect(this.ironWorker);
-        expect(this.player1).not.toBeAbleToSelect(this.stormwindSniper);
-    });
-
-    it('is not concealed from unit attacks when exhausted', function () {
-        exhaustCard(this.stormwindSniper, this.game);
-
-        this.player1.clickPrompt('Attack');
-        expect(this.player1).toHavePrompt('Select a target to attack');
-        // concealed!
-        expect(this.player1).toBeAbleToSelect(this.ironWorker);
-        expect(this.player1).toBeAbleToSelect(this.stormwindSniper);
-    });
-
-    function exhaustCard(card, game) {
-        game.actions.exhaust().resolve(card, game.getFrameworkContext());
-    }
 });
