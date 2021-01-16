cde7c49f6320759ece4b25707fe4dca8f6ebf480
kleer1 <kevin.l.ray@gmail.com>
2021-01-16 15:48:43 -0600

    update end of turn confirmation to only trigger if main action has not been used. updated verbiage of confirmation

--- server/game/gamesteps/main/actionwindow.js
+++ server/game/gamesteps/main/actionwindow.js
@@ -145,10 +145,10 @@ class ActionWindow extends UiPrompt {
 
         if (choice === 'done') {
             let cards = player.cardsInPlay.concat(player.hand);
-            if (cards.some((card) => card.getLegalActions(player).length > 0)) {
+            if (player.actions.main && cards.some((card) => card.getLegalActions(player).length > 0)) {
                 this.game.promptWithHandlerMenu(player, {
                     source: 'End Turn',
-                    activePromptTitle: 'Are you sure you want to end your turn?',
+                    activePromptTitle: 'Pass your main action?',
                     choices: ['Yes', 'No'],
                     handlers: [() => this.endActionWindow(), () => true]
                 });
