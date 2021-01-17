6bba4031d2c9a836667aad5d8f5936c5be483efe
kleer1 <kevin.l.ray@gmail.com>
2021-01-17 14:36:50 -0600

    use existing test case instead of adding more

--- test/server/cards/Ashes-Core/Empower.spec.js
+++ test/server/cards/Ashes-Core/Empower.spec.js
@@ -1,126 +1,31 @@
 describe('Empower', function () {
-    describe('unfocused', function () {
-        beforeEach(function () {
-            this.setupTest({
-                player1: {
-                    phoenixborn: 'coal-roarkwin',
-                    inPlay: ['iron-worker'],
-                    dicepool: ['natural', 'ceremonial', 'charm', 'charm'],
-                    hand: [],
-                    spellboard: ['empower']
-                },
-                player2: {
-                    phoenixborn: 'aradel-summergaard',
-                    inPlay: ['blue-jaguar', 'mist-spirit'],
-                    spellboard: ['summon-butterfly-monk']
-                }
-            });
-        });
-
-        it('adds a token to one unit', function () {
-            expect(this.ironWorker.tokens.status).toBeUndefined();
-
-            this.player1.clickCard(this.empower);
-            this.player1.clickPrompt('Empower');
-            this.player1.clickDie(0);
-            expect(this.player1).toHavePrompt('Choose a unit to empower');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.ironWorker.status).toBe(1);
-            expect(this.player1).toHavePrompt('Choose a card to play or use');
+    beforeEach(function () {
+        this.setupTest({
+            player1: {
+                phoenixborn: 'coal-roarkwin',
+                inPlay: ['iron-worker'],
+                dicepool: ['natural', 'ceremonial', 'charm', 'charm'],
+                hand: [],
+                spellboard: ['empower']
+            },
+            player2: {
+                phoenixborn: 'aradel-summergaard',
+                inPlay: ['blue-jaguar', 'mist-spirit'],
+                spellboard: ['summon-butterfly-monk']
+            }
         });
     });
 
-    describe('focus 1', function () {
-        beforeEach(function () {
-            this.setupTest({
-                player1: {
-                    phoenixborn: 'coal-roarkwin',
-                    inPlay: ['iron-worker'],
-                    dicepool: ['natural', 'ceremonial', 'charm', 'charm'],
-                    spellboard: ['empower', 'empower']
-                },
-                player2: {
-                    phoenixborn: 'aradel-summergaard',
-                    inPlay: ['iron-rhino']
-                }
-            });
-        });
+    it('adds a token to one unit', function () {
+        expect(this.ironWorker.tokens.status).toBeUndefined();
 
-        it('deals 1 damage to a unit', function () {
-            expect(this.ironWorker.tokens.status).toBeUndefined();
+        this.player1.clickCard(this.empower);
+        this.player1.clickPrompt('Empower');
+        this.player1.clickDie(0);
+        expect(this.player1).toHavePrompt('Choose a unit to empower');
+        this.player1.clickCard(this.ironWorker);
 
-            this.player1.clickCard(this.empower);
-            this.player1.clickPrompt('Empower');
-            this.player1.clickDie(0);
-            expect(this.player1).toHavePrompt('Choose a unit to empower');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.ironWorker.status).toBe(1);
-            expect(this.player1).toHavePrompt('Choose a unit with status tokens');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.player1).toHavePrompt('how many tokens?');
-            this.player1.clickPrompt('1');
-
-            expect(this.player1).toHavePrompt('Choose a unit to damage');
-            this.player1.clickCard(this.ironRhino);
-
-            expect(this.ironWorker.status).toBe(0);
-            expect(this.ironRhino.damage).toBe(1);
-        });
-
-        it('deals 2 damage to a unit', function () {
-            this.ironWorker.tokens.status = 2;
-
-            this.player1.clickCard(this.empower);
-            this.player1.clickPrompt('Empower');
-            this.player1.clickDie(0);
-            expect(this.player1).toHavePrompt('Choose a unit to empower');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.ironWorker.status).toBe(3);
-            expect(this.player1).toHavePrompt('Choose a unit with status tokens');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.player1).toHavePrompt('how many tokens?');
-            this.player1.clickPrompt('2');
-
-            expect(this.player1).toHavePrompt('Choose a unit to damage');
-            this.player1.clickCard(this.ironRhino);
-
-            expect(this.ironWorker.status).toBe(1);
-            expect(this.ironRhino.damage).toBe(2);
-        });
-
-        it('focus ability is cancelable', function () {
-            this.player1.clickCard(this.empower);
-            this.player1.clickPrompt('Empower');
-            this.player1.clickDie(0);
-            expect(this.player1).toHavePrompt('Choose a unit to empower');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.ironWorker.status).toBe(1);
-            expect(this.player1).toHavePrompt('Choose a unit with status tokens');
-            this.player1.clickPrompt('Done');
-            expect(this.player1).toHavePrompt('Choose a card to play or use');
-        });
-
-        it('ability bails if 0 tokens removed', function () {
-            this.player1.clickCard(this.empower);
-            this.player1.clickPrompt('Empower');
-            this.player1.clickDie(0);
-            expect(this.player1).toHavePrompt('Choose a unit to empower');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.ironWorker.status).toBe(1);
-            expect(this.player1).toHavePrompt('Choose a unit with status tokens');
-            this.player1.clickCard(this.ironWorker);
-
-            expect(this.player1).toHavePrompt('how many tokens?');
-            this.player1.clickPrompt('0');
-
-            expect(this.player1).toHavePrompt('Choose a card to play or use');
-        });
+        expect(this.ironWorker.status).toBe(1);
+        expect(this.player1).toHavePrompt('Choose a card to play or use');
     });
 });
