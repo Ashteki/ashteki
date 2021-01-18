3f3f2640b99d4825670d1154fda6a9adcb4b98fe
kleer1 <kevin.l.ray@gmail.com>
2021-01-17 22:37:30 -0600

    more tests to cover turn ending and buff applied after inversion

--- test/server/cards/Ashes-Core/BodyInversion.spec.js
+++ test/server/cards/Ashes-Core/BodyInversion.spec.js
@@ -11,8 +11,9 @@ describe('Body Inversion', function () {
             player2: {
                 phoenixborn: 'maeoni-viper',
                 inPlay: ['anchornaut', 'frost-fang', 'silver-snake'],
-                dicepool: ['illusion'],
-                spellboard: ['body-inversion']
+                dicepool: ['illusion', 'natural', 'natural'],
+                spellboard: ['body-inversion'],
+                hand: ['crystal-shield']
             }
         });
     });
@@ -54,6 +55,27 @@ describe('Body Inversion', function () {
         expect(this.anchornaut.life).toBe(2);
     });
 
+    it('flips only printed attack and life with buff applied after', function () {
+        // now body invert it. the buff from the shield should stay on the life
+        this.player1.clickCard(this.bodyInversion);
+        this.player1.clickPrompt('Body Inversion a unit');
+        this.player1.clickCard(this.frostFang);
+
+        expect(this.frostFang.attack).toBe(1);
+        expect(this.frostFang.life).toBe(3);
+
+        // Put a Crystal Shield on the unit to give a persistent buff
+        this.player1.clickCard(this.crystalShield);
+        this.player1.clickPrompt('Play This Alteration');
+        this.player1.clickDie(2);
+        this.player1.clickPrompt('Done');
+        this.player1.clickCard(this.frostFang);
+
+        expect(this.frostFang.location).toBe('play area');
+        expect(this.frostFang.attack).toBe(1);
+        expect(this.frostFang.life).toBe(5);
+    });
+
     it('interation with silver snake', function () {
         this.player1.clickCard(this.crimsonBomber);
         this.player1.clickPrompt('Detonate 3');
@@ -80,14 +102,6 @@ describe('Body Inversion', function () {
     });
 
     it('interation with silver snake and crystal shield', function () {
-        this.player1.clickCard(this.crystalShield);
-        this.player1.clickPrompt('Play This Alteration');
-        this.player1.clickDie(2);
-        this.player1.clickPrompt('Done');
-        this.player1.clickCard(this.silverSnake);
-
-        expect(this.silverSnake.life).toBe(6);
-
         this.player1.clickCard(this.crimsonBomber);
         this.player1.clickPrompt('Detonate 3');
         this.player1.clickPrompt('Done');
@@ -102,6 +116,15 @@ describe('Body Inversion', function () {
         this.player2.clickCard(this.silverSnake);
 
         expect(this.silverSnake.location).toBe('play area');
+        expect(this.silverSnake.attack).toBe(4);
+        expect(this.silverSnake.life).toBe(1);
+
+        this.player2.clickCard('crystal-shield', 'any', 'self');
+        this.player2.clickPrompt('Play This Alteration');
+        this.player2.clickDie(2);
+        this.player2.clickPrompt('Done');
+        this.player2.clickCard(this.silverSnake);
+
         expect(this.silverSnake.attack).toBe(4);
         expect(this.silverSnake.life).toBe(3);
 
