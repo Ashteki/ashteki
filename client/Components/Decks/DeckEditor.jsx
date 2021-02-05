ec7aa4e613d5ecaa12907fe893db656477fd9b96
Dan Johnson <danj3000@outlook.com>
2021-02-05 17:03:25 +0000

    conjurations note in deckeditor

--- client/Components/Decks/DeckEditor.jsx
+++ client/Components/Decks/DeckEditor.jsx
@@ -319,7 +319,7 @@ class InnerDeckEditor extends React.Component {
                     </Form.Group>
                     <h4>
                         You can type card names and quantities into the box below, or add them using
-                        this lookup box.
+                        this lookup box. DON&apos;T FORGET to add your CONJURATIONS!
                     </h4>
 
                     <Form.Group as={Row} controlId='cardLookup'>
