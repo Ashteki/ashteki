2c7ad8e3e21d767119174a10ec13738298bb4d54
Dan Johnson <danj3000@outlook.com>
2021-02-05 10:07:49 +0000

    summon squall stallion

---
+++ server/game/cards/Ashes-Core/SummonSquallStallion.js
@@ -0,0 +1,35 @@
+const { Level, Magic } = require('../../../constants.js');
+const Card = require('../../Card.js');
+const DiceCount = require('../../DiceCount.js');
+
+class SummonSquallStallion extends Card {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Summon Squall Stallion',
+            cost: [
+                ability.costs.mainAction(),
+                ability.costs.exhaust(),
+                ability.costs.dice([
+                    new DiceCount(1, Level.Power, Magic.Sympathy),
+                    new DiceCount(1, Level.Basic)
+                ])
+            ],
+            location: 'spellboard',
+            gameAction: ability.actions.draw(),
+            then: {
+                alwaysTriggers: true,
+                target: {
+                    controller: 'self',
+                    cardType: 'Conjuration',
+                    cardCondition: (card) => card.id === 'squall-stallion',
+                    location: 'archives',
+                    gameAction: ability.actions.putIntoPlay()
+                }
+            }
+        });
+    }
+}
+
+SummonSquallStallion.id = 'summon-squall-stallion';
+
+module.exports = SummonSquallStallion;
