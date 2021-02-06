576d15c45fb10eb601ebffd9e01a22868e39d3e1
Dan Johnson <danj3000@outlook.com>
2021-02-06 19:34:34 +0000

    close combat messaging

--- server/game/cards/Ashes-Core/CloseCombat.js
+++ server/game/cards/Ashes-Core/CloseCombat.js
@@ -5,12 +5,14 @@ class CloseCombat extends Card {
         this.play({
             targets: {
                 myChar: {
+                    activePromptTitle: 'Select a card for attack value',
                     cardType: ['Ally', 'Conjuration'],
                     controller: 'self',
                     cardCondition: (card) => !card.exhausted
                 },
                 oppChar: {
                     dependsOn: 'myChar',
+                    activePromptTitle: 'Choose a unit to damage',
                     cardType: ['Ally', 'Conjuration'],
                     controller: 'opponent',
                     gameAction: ability.actions.dealDamage((context) => ({
