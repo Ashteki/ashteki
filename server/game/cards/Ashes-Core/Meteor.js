ee78f859aaf200286a8ad058087ed671dd6bd48c
Dan Johnson <danj3000@outlook.com>
2021-02-10 20:57:44 +0000

    meteor part 2 - dice cost dependency in card

--- server/game/cards/Ashes-Core/Meteor.js
+++ server/game/cards/Ashes-Core/Meteor.js
@@ -1,3 +1,4 @@
+const { Level } = require('../../../constants.js');
 const Card = require('../../Card.js');
 
 class Meteor extends Card {
@@ -6,11 +7,20 @@ class Meteor extends Card {
 
         this.play({
             title: 'Meteor',
-            effect: 'deal 1 damage to all units - ADDITIONAL LION DAMAGE IS MANUAL',
+            effect: 'deal 1 damage to all units',
             gameAction: ability.actions.dealDamage((context) => ({
                 amount: 1,
                 target: context.game.unitsInPlay
-            }))
+            })),
+            then: {
+                gameAction: ability.actions.dealDamage((context) => {
+                    let diceCost = context.preThenEvent.context.event.context.costs.returnDice;
+                    return {
+                        amount: diceCost.filter((d) => d.level === Level.Power).length,
+                        target: context.game.unitsInPlay
+                    };
+                })
+            }
         });
     }
 }
