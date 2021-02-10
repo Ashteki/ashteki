2ab4b21cf014eb4773b68cd81e0a8671bbc1b57b
Dan Johnson <danj3000@outlook.com>
2021-02-10 19:36:51 +0000

    meteor manual dice

--- server/game/cards/Ashes-Core/Meteor.js
+++ server/game/cards/Ashes-Core/Meteor.js
@@ -2,6 +2,8 @@ const Card = require('../../Card.js');
 
 class Meteor extends Card {
     setupCardAbilities(ability) {
+        this.preventAutoDice = true;
+
         this.play({
             title: 'Meteor',
             effect: 'deal 1 damage to all units - ADDITIONAL LION DAMAGE IS MANUAL',
