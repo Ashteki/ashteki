299e1b3bfa56f034d54421eb52d1b5c0768e688a
Dan Johnson <danj3000@outlook.com>
2021-03-10 11:26:06 +0000

    rewrite playerboard as functional component

--- client/Components/GameBoard/PlayerBoard.jsx
+++ client/Components/GameBoard/PlayerBoard.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import Card from './Card';
 
 import './PlayerBoard.scss';
 
-class PlayerBoard extends React.Component {
-    attackInvolvesCard(card) {
-        let attack = this.props.attack;
+const PlayerBoard = ({
+    active,
+    attack,
+    cardBackUrl,
+    cardsInPlay,
+    cardSize,
+    manualMode,
+    onCardClick,
+    onMenuItemClick,
+    onMouseOut,
+    onMouseOver,
+    playerId,
+    rowDirection,
+    side
+}) => {
+    const attackInvolvesCard = (card) => {
         if (!attack) return false;
 
         return attack.battles.some(
@@ -17,41 +29,40 @@ class PlayerBoard extends React.Component {
                 (b.target === card.uuid && !b.guard) ||
                 b.guard === card.uuid
         );
-    }
+    };
 
-    getCard(uuid) {
-        return this.props.cardsInPlay.find((c) => c.uuid === uuid);
-    }
+    const getCard = (uuid) => {
+        return cardsInPlay.find((c) => c.uuid === uuid);
+    };
 
-    renderRow(row) {
+    const renderRow = (row) => {
         const results = [];
         // render attack cards or gaps
-        let attack = this.props.attack;
         if (attack)
             // should we display the attackers or defenders?
             // is this player (top or bottom) the attacker?
             attack.battles.map((b) => {
-                if (attack.attackingPlayer === this.props.playerId) {
+                if (attack.attackingPlayer === playerId) {
                     // just display the attackers
-                    const attackerCard = this.getCard(b.attacker);
-                    results.push(this.renderCard(attackerCard));
+                    const attackerCard = getCard(b.attacker);
+                    results.push(renderCard(attackerCard));
                 } else {
                     // work out defender or blank
-                    let defCard = this.getCard(b.guard) || this.getCard(b.target);
-                    if (defCard) results.push(this.renderCard(defCard));
-                    else results.push(this.renderCardGap());
+                    let defCard = getCard(b.guard) || getCard(b.target);
+                    if (defCard) results.push(renderCard(defCard));
+                    else results.push(renderCardGap());
                 }
             });
 
         // render the remaining cards not involved in the attack
-        let rowCards = row.filter((c) => !this.attackInvolvesCard(c));
-        const renderedCards = rowCards.map((card) => this.renderCard(card));
+        let rowCards = row.filter((c) => !attackInvolvesCard(c));
+        const renderedCards = rowCards.map((card) => renderCard(card));
         return results.concat(renderedCards);
-    }
+    };
 
-    renderCardGap() {
+    const renderCardGap = () => {
         const sizeClass = {
-            [this.props.cardSize]: this.props.cardSize !== 'normal'
+            [cardSize]: cardSize !== 'normal'
         };
         let cardClass = classNames('game-card', 'vertical', sizeClass);
 
@@ -62,60 +73,44 @@ class PlayerBoard extends React.Component {
                 </div>
             </div>
         );
-    }
+    };
 
-    renderCard(card) {
+    const renderCard = (card) => {
         if (!card) return;
         return (
             <Card
                 key={card.uuid}
-                cardBackUrl={this.props.cardBackUrl}
-                canDrag={this.props.manualMode}
+                cardBackUrl={cardBackUrl}
+                canDrag={manualMode}
                 card={card}
                 disableMouseOver={card.facedown && !card.code}
-                onClick={this.props.onCardClick}
-                onMenuItemClick={this.props.onMenuItemClick}
-                onMouseOut={this.props.onMouseOut}
-                onMouseOver={this.props.onMouseOver}
-                size={this.props.cardSize}
+                onClick={onCardClick}
+                onMenuItemClick={onMenuItemClick}
+                onMouseOut={onMouseOut}
+                onMouseOver={onMouseOver}
+                size={cardSize}
                 source='play area'
-                side={this.props.side}
+                side={side}
             />
         );
-    }
+    };
 
-    render() {
-        let className = classNames('player-board', {
-            'our-side': this.props.rowDirection === 'default',
-            'active-player': this.props.active
-        });
+    let className = classNames('player-board', {
+        'our-side': rowDirection === 'default',
+        'active-player': active
+    });
 
-        let maxUpgrades = Math.max(...this.props.cardsInPlay.map((c) => c.upgrades.length));
-        let topMargin = maxUpgrades * 15;
-        let style = { 'margin-top': topMargin + 'px' };
-        return (
-            <div className={className}>
-                <div className='card-row' style={style}>
-                    {this.renderRow(this.props.cardsInPlay)}
-                </div>
+    let maxUpgrades = Math.max(...cardsInPlay.map((c) => c.upgrades.length));
+    let topMargin = maxUpgrades * 15;
+    let style = { 'margin-top': topMargin + 'px' };
+
+    return (
+        <div className={className}>
+            <div className='card-row' style={style}>
+                {renderRow(cardsInPlay)}
             </div>
-        );
-    }
-}
-
-PlayerBoard.displayName = 'PlayerBoard';
-PlayerBoard.propTypes = {
-    cardBackUrl: PropTypes.string,
-    cardsInPlay: PropTypes.array,
-    manualMode: PropTypes.bool,
-    onCardClick: PropTypes.func,
-    onMenuItemClick: PropTypes.func,
-    onMouseOut: PropTypes.func,
-    onMouseOver: PropTypes.func,
-    rowDirection: PropTypes.oneOf(['default', 'reverse']),
-    cardSize: PropTypes.string,
-    playerId: PropTypes.string,
-    active: PropTypes.bool
+        </div>
+    );
 };
 
 export default PlayerBoard;
