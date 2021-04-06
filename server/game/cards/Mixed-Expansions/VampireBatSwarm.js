4cb870534e4668030ed0d59e8df86d8e5c6580ee
Dan Johnson <danj3000@outlook.com>
2021-04-06 11:20:18 +0100

    vampire bat swarm fix

--- server/game/cards/Mixed-Expansions/VampireBatSwarm.js
+++ server/game/cards/Mixed-Expansions/VampireBatSwarm.js
@@ -6,16 +6,28 @@ class VampireBatSwarm extends Card {
     setupCardAbilities(ability) {
         this.groupTactics({ amount: 1 });
 
-        this.destroyed({
-            cost: ability.costs.dice([
+        this.forcedInterrupt({
+            autoResolve: true,
+            when: {
+                onCardLeavesPlay: (event, context) =>
+                    event.triggeringEvent.name === 'onCardDestroyed' &&
+                    event.card === context.source
+            },
+            cost: ability.costs.dice(
                 [
-                    new DiceCount(1, Level.Class, Magic.Ceremonial),
-                    new DiceCount(1, Level.Class, Magic.Sympathy)
-                ]
-            ]),
-            gameAction: ability.actions.putIntoPlay({
-                target: this
-            })
+                    [
+                        new DiceCount(1, Level.Class, Magic.Ceremonial),
+                        new DiceCount(1, Level.Class, Magic.Sympathy)
+                    ]
+                ],
+                'Activate Swarm?'
+            ),
+            gameAction: ability.actions.changeEvent((context) => ({
+                event: context.event,
+                destination: 'play area',
+                cancel: false
+            })),
+            effect: 'place {0} on their battlefield'
         });
     }
 }
