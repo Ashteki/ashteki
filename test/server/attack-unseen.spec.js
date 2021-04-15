a41da8c48b8686434d7eb1cdff626b26bfd07b4f
Dan Johnson <danj3000@outlook.com>
2021-04-15 09:20:18 +0100

    additional test for unseen - bug reported but not found

--- test/server/attack-unseen.spec.js
+++ test/server/attack-unseen.spec.js
@@ -1,4 +1,56 @@
 describe('unseen', function () {
+    describe('does not stop blocking when the only unit attacking is unseen', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['iron-worker', 'mind-fog-owl'],
+                    dicepool: ['natural', 'natural', 'charm', 'charm']
+                },
+                player2: {
+                    phoenixborn: 'coal-roarkwin',
+                    inPlay: ['anchornaut', 'hammer-knight', 'gilder']
+                }
+            });
+        });
+
+        it('single pb attacker', function () {
+            this.player1.clickAttack(this.coalRoarkwin);
+            this.player1.clickCard(this.mindFogOwl);
+            this.player1.clickDone();
+            expect(this.player2).toHavePrompt('Choose a blocker');
+
+            this.player2.clickCard(this.anchornaut);
+            expect(this.player2).toBeAbleToSelect(this.mindFogOwl);
+
+            this.player2.clickCard(this.mindFogOwl);
+            expect(
+                this.game.attackState.battles.some((b) => b.attacker === this.mindFogOwl && b.guard)
+            ).toBe(true);
+            this.player2.clickDone();
+
+            expect(this.player1).toHaveDefaultPrompt();
+        });
+
+        it('unit attack', function () {
+            this.player1.clickAttack(this.anchornaut);
+            this.player1.clickCard(this.mindFogOwl);
+
+            expect(this.player2).toHavePrompt('Choose a guard?');
+
+            expect(this.player2).toBeAbleToSelect(this.gilder);
+            this.player2.clickCard(this.gilder);
+
+            expect(
+                this.game.attackState.battles.some((b) => b.attacker === this.mindFogOwl && b.guard)
+            ).toBe(true);
+
+            this.player2.clickDone(); // gilder
+
+            expect(this.player1).toHaveDefaultPrompt();
+        });
+    });
+
     describe('must block non-unseen defenders first', function () {
         beforeEach(function () {
             this.setupTest({
