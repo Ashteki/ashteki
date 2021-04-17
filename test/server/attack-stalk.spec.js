253af06dfc258fe4a77689c9dbaa2d1af0a69f4c
Dan Johnson <danj3000@outlook.com>
2021-04-17 09:11:54 +0100

    extra stalk test goodness

--- test/server/attack-stalk.spec.js
+++ test/server/attack-stalk.spec.js
@@ -3,7 +3,7 @@ describe('stalk keyword', function () {
         this.setupTest({
             player1: {
                 phoenixborn: 'aradel-summergaard',
-                inPlay: ['shadow-hound', 'mist-spirit'],
+                inPlay: ['shadow-hound', 'mist-spirit', 'winged-lioness'],
                 spellboard: ['hypnotize'],
                 dicepool: ['natural', 'natural', 'charm', 'charm']
             },
@@ -23,6 +23,12 @@ describe('stalk keyword', function () {
         this.player1.clickCard(this.shadowHound);
         this.player1.clickPrompt('Done'); // single attacker
         expect(this.player2).toHavePrompt('Choose a blocker');
+        expect(this.player2).toBeAbleToSelect(this.anchornaut);
+        this.player2.clickCard(this.anchornaut);
+        this.player2.clickCard(this.shadowHound);
+        this.player2.clickDone();
+        expect(this.anchornaut.location).toBe('discard');
+        expect(this.player1).toHaveDefaultPrompt();
     });
 
     it('prevents guard', function () {
@@ -30,6 +36,29 @@ describe('stalk keyword', function () {
         expect(this.shadowHound.anyEffect('preventGuard')).toBe(true);
         this.player1.clickCard(this.anchornaut); // target
         this.player1.clickCard(this.shadowHound);
+        expect(this.player2).not.toHavePrompt('Choose a guard?');
+        expect(this.player2).toHavePrompt('Do you want to counter?');
+    });
+
+    it('does not prevent block - bug report', function () {
+        this.player1.clickPrompt('Attack');
+        this.player1.clickCard(this.coalRoarkwin); // target
+        this.player1.clickCard(this.wingedLioness);
+        this.player1.clickPrompt('Done'); // single attacker
+        expect(this.player2).toHavePrompt('Choose a blocker');
+        expect(this.player2).toBeAbleToSelect(this.anchornaut);
+        this.player2.clickCard(this.anchornaut);
+        this.player2.clickCard(this.wingedLioness);
+        this.player2.clickDone();
+        expect(this.anchornaut.location).toBe('discard');
+        expect(this.player1).toHaveDefaultPrompt();
+    });
+
+    it('prevents guard - bug report', function () {
+        this.player1.clickPrompt('Attack');
+        expect(this.shadowHound.anyEffect('preventGuard')).toBe(true);
+        this.player1.clickCard(this.anchornaut); // target
+        this.player1.clickCard(this.wingedLioness);
         expect(this.player2).not.toHavePrompt('Choose a guard?');
         expect(this.player2).toHavePrompt('Do you want to counter?');
     });
