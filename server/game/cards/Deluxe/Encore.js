71ca673a36cc296aa0971ef9f76aab7ad1a9d91e
Dan Johnson <danj3000@outlook.com>
2021-04-25 16:07:40 +0100

    remove AoE message from encore

--- server/game/cards/Deluxe/Encore.js
+++ server/game/cards/Deluxe/Encore.js
@@ -4,7 +4,6 @@ class Encore extends Card {
     setupCardAbilities(ability) {
         this.play({
             title: 'Encore',
-            effect: 'deal 1 damage to all units',
             targets: {
                 myCard: {
                     activePromptTitle: 'Select a card to return to your deck',
