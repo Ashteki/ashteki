a7601fff569803953eee770e2b0f6e26ce2ce2a3
Dan Johnson <danj3000@outlook.com>
2021-04-26 10:19:09 +0100

    Rimea fresh implementation per redKing suggestion cards are re-ordered THEN a cost is offered to place top card to bottom of deck

--- server/game/cards/Mixed-Expansions/RimeaCareworn.js
+++ server/game/cards/Mixed-Expansions/RimeaCareworn.js
@@ -1,15 +1,38 @@
+const { Level, Magic } = require('../../../constants.js');
 const Card = require('../../Card.js');
+const DiceCount = require('../../DiceCount.js');
 
 class RimeaCareworn extends Card {
     setupCardAbilities(ability) {
         this.action({
             title: 'Visions',
             cost: ability.costs.sideAction(),
+            target: {
+                mode: 'options',
+                activePromptTitle: "Reflect Sorrow: Choose which player's units",
+                options: [
+                    { name: 'Mine', value: false },
+                    { name: "Opponent's", value: true }
+                ],
+                handler: (option) => (this.chosenValue = option.value)
+            },
             gameAction: ability.actions.rearrangeCards((context) => ({
-                target: context.player.opponent,
-                purgeType: 'bottom',
-                purge: 1 // to bottom
+                target: this.chosenValue ? context.player.opponent : context.player,
+                purge: 0
             })),
+            then: {
+                cost: [
+                    ability.costs.dice([new DiceCount(1, Level.Basic)],
+                        'Move top of deck to the bottom?'
+                    )],
+                gameAction: ability.actions.moveToBottom((context) => ({
+                    target: this.chosenValue
+                        ? context.player.opponent.deck[0]
+                        : context.player.deck[0]
+                })),
+                message: '{0} moves 1 card from the top of their deck to the bottom',
+                messageArgs: (context) => [context.player]
+            },
             preferActionPromptMessage: true
         });
     }
