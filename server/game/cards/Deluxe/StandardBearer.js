5813700e8568cffacf78c38e758eb0cb49868eed
Dan Johnson <danj3000@outlook.com>
2021-05-04 14:12:49 +0100

    standard bearer

---
+++ server/game/cards/Deluxe/StandardBearer.js
@@ -0,0 +1,50 @@
+const { BattlefieldTypes } = require('../../../constants.js');
+const Card = require('../../Card.js');
+
+class StandardBearer extends Card {
+    setupCardAbilities(ability) {
+        this.entersPlay({
+            gameAction: ability.actions.addStatusToken((context) => ({
+                amount: 1,
+                target: context.source
+            }))
+        });
+
+        this.forcedReaction({
+            inexhaustible: true,
+            title: 'Resourceful 1',
+            autoResolve: true,
+            when: {
+                onPhaseStarted: (event) => event.phase === 'playerturns'
+            },
+            effect: 'gain 1 status token',
+            gameAction: ability.actions.addStatusToken()
+        });
+
+        this.forcedReaction({
+            title: 'Inspire 1',
+            when: {
+                onAttackersDeclared: (event, context) => {
+                    // I'm the attacker
+                    return event.battles.some((b) => b.attacker === context.source);
+                }
+            },
+            target: {
+                mode: 'upTo',
+                numCards: (context) => context.source.status,
+                optional: true,
+                cardType: BattlefieldTypes,
+                controller: 'self',
+                cardCondition: (card, context) => card !== context.source,
+                gameAction: ability.actions.cardLastingEffect({
+                    duration: 'untilEndOfTurn',
+                    effect: ability.effects.modifyAttack(1)
+                })
+            }
+        });
+    }
+}
+
+StandardBearer.id = 'standard-bearer';
+
+module.exports = StandardBearer;
