a0b4c579ebadb1310077627e28e58441da824351
Dan Johnson <danj3000@outlook.com>
2021-05-10 08:52:51 +0100

    standard bearer / selection while attacking UI fix

--- test/server/cards/StandardBearer.spec.js
+++ test/server/cards/StandardBearer.spec.js
@@ -3,7 +3,7 @@ describe('standard bearer', function () {
         this.setupTest({
             player1: {
                 phoenixborn: 'aradel-summergaard',
-                inPlay: ['standard-bearer', 'mist-spirit', 'iron-worker'],
+                inPlay: ['standard-bearer', 'mist-spirit', 'time-hopper', 'iron-worker'],
                 spellboard: ['summon-butterfly-monk']
             },
             player2: {
@@ -14,21 +14,25 @@ describe('standard bearer', function () {
                 hand: []
             }
         });
-        this.standardBearer.tokens.status = 2;
+        this.standardBearer.tokens.status = 4;
     });
 
     it('adds 1 to a unit for each status', function () {
         this.player1.clickPrompt('Attack');
         this.player1.clickCard(this.coalRoarkwin); // target
-        this.player1.clickCard(this.mistSpirit);
         this.player1.clickCard(this.standardBearer);
+        this.player1.clickCard(this.timeHopper);
+        this.player1.clickCard(this.mistSpirit);
         this.player1.clickPrompt('Done'); // done for attackers
         this.player1.clickCard(this.mistSpirit);
+        this.player1.clickCard(this.timeHopper);
         this.player1.clickDone();
+        expect(this.timeHopper.attack).toBe(2);
         expect(this.mistSpirit.attack).toBe(2);
         this.player2.clickDone();
-        this.player1.clickCard(this.mistSpirit);
+        this.player1.clickCard(this.standardBearer);
+        this.player1.clickCard(this.timeHopper);
         expect(this.player1).toHaveDefaultPrompt();
-        expect(this.coalRoarkwin.damage).toBe(3);
+        expect(this.coalRoarkwin.damage).toBe(5);
     });
 });
