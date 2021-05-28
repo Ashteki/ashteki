dfd08316877489e65f7203f5a05457a8f9f3e96b
Dan Johnson <danj3000@outlook.com>
2021-05-28 17:32:04 +0100

    hopeful rising horde fix (removed duplicate code and added test)

---
+++ test/server/cards/RisingHorde.spec.js
@@ -0,0 +1,86 @@
+describe('Rising Horde', function () {
+    describe('When Destroyed', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['seaside-raven', 'iron-worker'],
+                    dicepool: ['natural']
+                },
+                player2: {
+                    phoenixborn: 'maeoni-viper',
+                    inPlay: ['rising-horde', 'living-doll'],
+                    archives: ['fallen', 'fallen', 'fallen']
+                }
+            });
+        });
+
+        it('places two fallen into play', function () {
+            this.player1.clickPrompt('Attack');
+            this.player1.clickCard(this.risingHorde);
+            this.player1.clickCard(this.ironWorker);
+
+            this.player2.clickPrompt('Done'); // no blocker
+            this.player2.clickPrompt('No'); // no counter
+
+            expect(this.player2).toHavePrompt('Rising Horde');
+            this.player2.clickCard(this.player2.archives[0]);
+            this.player2.clickCard(this.player2.archives[1]);
+            this.player2.clickDone();
+
+            expect(this.player1).toHaveDefaultPrompt();
+
+            expect(this.player2.inPlay.length).toBe(3);
+            expect(this.risingHorde.location).toBe('discard');
+        });
+    });
+
+    describe('with summon sleeping widows', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['seaside-raven', 'iron-worker'],
+                    dicepool: ['natural']
+                },
+                player2: {
+                    phoenixborn: 'maeoni-viper',
+                    inPlay: ['rising-horde', 'living-doll'],
+                    archives: [
+                        'fallen',
+                        'fallen',
+                        'fallen',
+                        'sleeping-widow',
+                        'sleeping-widow',
+                        'sleeping-widow'
+                    ],
+                    dicepool: ['ceremonial', 'ceremonial'],
+                    hand: ['summon-sleeping-widows']
+                }
+            });
+        });
+
+        it('places two fallen into play', function () {
+            this.player1.clickPrompt('Attack');
+            this.player1.clickCard(this.risingHorde);
+            this.player1.clickCard(this.ironWorker);
+
+            this.player2.clickPrompt('Done'); // no blocker
+            this.player2.clickPrompt('No'); // no counter
+
+            expect(this.player2).toHavePrompt('Rising Horde');
+            this.player2.clickCard(this.player2.archives[0]);
+            this.player2.clickCard(this.player2.archives[1]);
+            this.player2.clickDone();
+
+            expect(this.player1).not.toHaveDefaultPrompt();
+            this.player2.clickCard(this.summonSleepingWidows);
+            this.player2.clickCard(this.player2.archives[1]);
+            this.player2.clickCard(this.player2.archives[2]);
+            this.player2.clickDone();
+
+            expect(this.player2.inPlay.length).toBe(4); // maeoni max
+            expect(this.risingHorde.location).toBe('discard');
+        });
+    });
+});
