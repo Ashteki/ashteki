2b7b3a3a05b8f40f3ca6f1dc0031b7203035303b
Dan Johnson <danj3000@outlook.com>
2021-06-09 14:49:51 +0100

    fix for psychic vampire

--- server/game/cards/Mixed-Expansions/PsychicVampire.js
+++ server/game/cards/Mixed-Expansions/PsychicVampire.js
@@ -4,7 +4,7 @@ class PsychicVampire extends Card {
     setupCardAbilities(ability) {
         this.destroyed({
             condition: (context) =>
-                context.event.context.player === context.source.controller.opponent,
+                context.event.damageEvent.damageSource.controller === context.source.controller.opponent,
             gameAction: ability.actions.chosenDiscard((context) => ({
                 target: context.player.opponent
             }))
---
+++ test/server/cards/PsychicVampire.spec.js
@@ -0,0 +1,49 @@
+describe('psychic vampire', function () {
+    describe('psychic vampire ability', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['rose-fire-dancer'],
+                    spellboard: [],
+                    dicepool: ['natural', 'natural', 'charm', 'charm'],
+                    archives: [],
+                    hand: ['molten-gold', 'iron-worker']
+                },
+                player2: {
+                    phoenixborn: 'rin-northfell',
+                    inPlay: ['psychic-vampire'],
+                    spellboard: [],
+                    hand: ['rins-fury'],
+                    dicepool: ['natural', 'natural']
+                }
+            });
+        });
+
+        it('ability places exhaustion token', function () {
+            this.player1.clickCard(this.aradelSummergaard);
+            this.player1.clickPrompt('water blast');
+            this.player1.clickCard(this.psychicVampire);
+
+            this.player1.clickCard(this.ironWorker);
+
+            expect(this.psychicVampire.location).toBe('discard');
+            expect(this.ironWorker.location).toBe('discard');
+        });
+
+        it('ability triggers on counter', function () {
+            this.player1.endTurn();
+            this.player2.clickAttack(this.roseFireDancer);
+            this.player2.clickCard(this.psychicVampire);
+
+            this.player1.clickDone();
+            this.player1.clickYes();
+
+            this.player1.clickCard(this.ironWorker);
+
+            expect(this.psychicVampire.location).toBe('discard');
+            expect(this.ironWorker.location).toBe('discard');
+        });
+
+    });
+});
