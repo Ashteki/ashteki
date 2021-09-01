3e94e75d4079154204b4a899ff18232d4427ac44
5H4Z <89332227+5H4Z@users.noreply.github.com>
2021-09-01 22:25:14 +1000

    Change to independent effects for BUG-519

--- server/game/cards/Mixed-Expansions/ReapingAngel.js
+++ server/game/cards/Mixed-Expansions/ReapingAngel.js
@@ -5,30 +5,31 @@ class ReapingAngel extends Card {
         this.entersPlay({
             target: {
                 optional: true,
-                activePromptTitle: 'Choose a card to discard',
+                activePromptTitle: 'Choose an ally to discard',
                 location: 'deck',
                 controller: 'self',
+		        cardType: 'Ally',
                 gameAction: [
                     ability.actions.reveal(),
                     ability.actions.moveCard({ destination: 'discard' }),
                     ability.actions.shuffleDeck()
                 ]
-            },
-            then: {
-                alwaysTriggers: true,
-                target: {
-                    activePromptTitle: 'Choose a card to remove from the game',
-                    optional: true,
-                    location: 'discard',
-                    controller: 'self',
-                    gameAction: [
-                        ability.actions.purge(),
-                        ability.actions.removeDamage((context) => ({
-                            target: context.player.phoenixborn,
-                            showMessage: true
-                        }))
-                    ]
-                }
+            }
+        });
+        this.entersPlay({
+            target: {
+                activePromptTitle: 'Choose an ally to remove from the game',
+                optional: true,
+                location: 'discard',
+                controller: 'self',
+                cardType: 'Ally',
+                gameAction: [
+                    ability.actions.purge(),
+                    ability.actions.removeDamage((context) => ({
+                        target: context.player.phoenixborn,
+                        showMessage: true
+                    }))
+                ]
             }
         });
     }
