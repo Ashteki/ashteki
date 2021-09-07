0b409a1c133ecb7c03b0aa88c1355f501a04c4ec
5H4Z <89332227+5H4Z@users.noreply.github.com>
2021-09-07 12:47:31 +1000

    Fate Reflection implementation and tests

---
+++ test/server/cards/FateReflection.spec.js
@@ -0,0 +1,60 @@
+describe('Fate Reflection reaction spell', function () {
+    beforeEach(function () {
+        this.setupTest({
+            player1: {
+                phoenixborn: 'aradel-summergaard',
+                inPlay: ['flute-mage', 'hammer-knight'],
+                spellboard: [],
+                hand: ['anchornaut'],
+                dicepool: ['natural', 'natural', 'charm', 'charm']
+            },
+            player2: {
+                phoenixborn: 'coal-roarkwin',
+                inPlay: ['iron-worker', 'mist-spirit'],
+                spellboard: ['summon-butterfly-monk'],
+                dicepool: ['natural', 'natural', 'charm', 'charm', 'time'],
+                archives: ['butterfly-monk'],
+                hand: ['fate-reflection']
+            }
+        });
+    });
+
+    it('can be played when a unit takes damage', function () {
+        expect(this.ironWorker.tokens.damage).toBeUndefined;
+
+        this.player1.clickCard(this.anchornaut);
+        this.player1.clickPrompt('Play This Ally');
+        this.player1.clickDie(2);
+        this.player1.clickCard(this.ironWorker); // target
+
+        // any interrupts?
+        this.player2.clickCard(this.fateReflection); // click fate reflection to play as reaction
+        this.player2.clickDie(4);
+        this.player2.clickCard(this.hammerKnight); // deal damage to hammerKnight
+
+        expect(this.fateReflection.location).toBe('discard');
+        expect(this.player2.hand.length).toBe(0);
+
+        expect(this.hammerKnight.tokens.damage).toBe(1); // apply correct damage
+        expect(this.ironWorker.tokens.damage).toBeUndefined;
+    });
+
+    it('can be played when a unit takes Water Blast damage', function () {
+        expect(this.ironWorker.tokens.damage).toBeUndefined;
+
+        this.player1.clickCard(this.aradelSummergaard);
+        this.player1.clickPrompt('Water Blast');
+        this.player1.clickCard(this.ironWorker);
+
+        // any interrupts?
+        this.player2.clickCard(this.fateReflection); // click fate reflection to play as reaction
+        this.player2.clickDie(4);
+        this.player2.clickCard(this.hammerKnight); // deal damage to hammerKnight
+
+        expect(this.fateReflection.location).toBe('discard');
+        expect(this.player2.hand.length).toBe(0);
+
+        expect(this.hammerKnight.tokens.damage).toBe(2); // apply correct damage
+        expect(this.ironWorker.tokens.damage).toBeUndefined;
+    });
+});
