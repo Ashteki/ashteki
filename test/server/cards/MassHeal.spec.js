162cc9da2b016d3a37efc83af0ccda1daa6bc651
5H4Z <89332227+5H4Z@users.noreply.github.com>
2021-09-11 22:33:52 +1000

    Make Mass Heal optional

--- test/server/cards/MassHeal.spec.js
+++ test/server/cards/MassHeal.spec.js
@@ -24,11 +24,23 @@ describe('Mass Heal action spell', function () {
     it('heals all MY units plus pb', function () {
         this.player1.clickCard(this.massHeal);
         this.player1.clickPrompt('Play this action');
+        this.player1.clickPrompt('Heal your units and PB');
 
         expect(this.hammerKnight.damage).toBe(0);
         expect(this.silverSnake.damage).toBe(1);
         expect(this.aradelSummergaard.damage).toBe(1);
         expect(this.odetteDiamondcrest.damage).toBe(0);
+    });
+
+    it('heals all units (no PB) including opponents if I select the option', function () {
+        this.player1.clickCard(this.massHeal);
+        this.player1.clickPrompt('Play this action');
+        this.player1.clickPrompt('Remove wounds from all units');
+
+        expect(this.hammerKnight.damage).toBe(0);
+        expect(this.silverSnake.damage).toBe(0);
+        expect(this.aradelSummergaard.damage).toBe(1);
+        expect(this.odetteDiamondcrest.damage).toBe(1);
     });
 
     it('heals all units (no PB) including opponents if no divine power/class', function () {
