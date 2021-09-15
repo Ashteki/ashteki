d91e8edc7d839121b4bd34bd09315980f327b7ca
5H4Z <89332227+5H4Z@users.noreply.github.com>
2021-09-15 21:19:45 +1000

    Minor message updates

--- server/game/cards/Mixed-Expansions/MassHeal.js
+++ server/game/cards/Mixed-Expansions/MassHeal.js
@@ -6,18 +6,31 @@ class MassHeal extends Card {
         this.play({
             title: 'Mass Heal',
             gameAction: ability.actions.conditional((context) => ({
-                // target: context.source.parent,
                 condition: () =>
                     context.player.dice.some(
                         (d) => d.magic === Magic.Divine && d.level !== Level.Basic && !d.exhausted
                     ),
-                trueGameAction: ability.actions.removeDamage({
-                    amount: 1,
-                    target: [context.player.phoenixborn, ...context.player.unitsInPlay]
-                }),
+                trueGameAction: ability.actions.chooseAction((context) => ({
+                    choices: {
+                        'Heal your units and PB': ability.actions.removeDamage({
+                            amount: 1,
+                            target: [context.player.phoenixborn, ...context.player.unitsInPlay]
+                        }),
+                        'Remove wounds from all units': ability.actions.removeDamage({
+                            amount: 1,
+                            target: context.game.unitsInPlay
+                        })
+                    },
+                    messages: {
+                        'Heal your units and PB': '{0} chooses to heal their units and PB',
+                        'Remove wounds from all units': '{0} chooses to heal all units'
+                    } // This gets into the ChooseGameAction function
+                })),
                 falseGameAction: ability.actions.removeDamage({
                     amount: 1,
-                    target: context.game.unitsInPlay
+                    target: context.game.unitsInPlay,
+                    message: '{0} heals all units', // this doesn't work as removeDamage doesn't pass the message on
+                    showMessage: true
                 })
             }))
         });
