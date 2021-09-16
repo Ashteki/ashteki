56212025eb2d022962084c59f288bddf5cfde67c
bwain <bwain@grubhub.com>
2021-09-15 23:19:26 -0500

    flash strike is after blockers are declared, not when

--- test/server/cards/FlashStrike.spec.js
+++ test/server/cards/FlashStrike.spec.js
@@ -3,14 +3,14 @@ describe('Flash Strike', function () {
         this.setupTest({
             player1: {
                 phoenixborn: 'aradel-summergaard',
-                inPlay: ['mist-spirit', 'iron-worker'],
+                inPlay: ['mist-spirit', 'iron-worker', 'time-hopper'],
                 spellboard: ['summon-butterfly-monk'],
                 dicepool: ['time', 'natural', 'illusion'],
                 hand: ['flash-strike']
             },
             player2: {
                 phoenixborn: 'coal-roarkwin',
-                inPlay: ['flute-mage'],
+                inPlay: ['flute-mage', 'sunshield-sentry'],
                 spellboard: [],
                 hand: ['summon-sleeping-widows'],
                 archives: ['sleeping-widow']
@@ -18,6 +18,7 @@ describe('Flash Strike', function () {
         });
 
         this.mistSpirit.tokens.status = 1;
+        this.timeHopper.tokens.status = 1;
     });
 
     it('reaction on guard choice - unit attack', function () {
@@ -27,6 +28,12 @@ describe('Flash Strike', function () {
         this.player2.clickDone(); // no guard
 
         this.player1.clickCard(this.flashStrike);
+
+        //needs status tokens
+        expect(this.player1).toBeAbleToSelect(this.mistSpirit);
+        expect(this.player1).toBeAbleToSelect(this.timeHopper);
+        expect(this.player1).not.toBeAbleToSelect(this.ironWorker);
+
         this.player1.clickCard(this.mistSpirit);
 
         expect(this.player1.dicepool[0].exhausted).toBe(true);
@@ -38,5 +45,41 @@ describe('Flash Strike', function () {
         expect(this.fluteMage.location).toBe('discard');
 
         expect(this.player1).toHaveDefaultPrompt();
+    });
+
+    it('occurs after WHEN blockers declared effects, such as sunshield sentry', function () {
+        this.player1.clickPrompt('Attack');
+        this.player1.clickCard(this.coalRoarkwin); // target
+        this.player1.clickCard(this.mistSpirit);
+        this.player1.clickCard(this.timeHopper);
+        this.player1.clickDone();
+
+        //block
+        this.player2.clickCard(this.sunshieldSentry);
+        this.player2.clickCard(this.mistSpirit);
+        this.player2.clickDone();
+
+        expect(this.player2).toBeAbleToSelect(this.sunshieldSentry); //any reactions
+        this.player2.clickCard(this.sunshieldSentry);
+        this.player2.clickCard(this.timeHopper);
+
+        expect(this.player1).toBeAbleToSelect(this.flashStrike); //any reactions
+
+        this.player1.clickCard(this.flashStrike);
+
+        //can do any status token unit, not just attackers
+        expect(this.player1).toBeAbleToSelect(this.mistSpirit);
+        expect(this.player1).toBeAbleToSelect(this.timeHopper);
+
+        this.player1.clickCard(this.mistSpirit);
+
+        expect(this.player1.dicepool[0].exhausted).toBe(true);
+
+        expect(this.mistSpirit.location).toBe('archives'); //it still died to sentry
+        expect(this.sunshieldSentry.damage).toBe(3);
+        expect(this.timeHopper.location).toBe('play area');
+        expect(this.timeHopper.exhausted).toBe(false);
+
+        expect(this.coalRoarkwin.damage).toBe(0);
     });
 });
