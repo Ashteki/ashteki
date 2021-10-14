fa560961cb3b75ff2aa732928b87b31fe5521a6a
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2021-10-14 19:09:55 +0100

    targetting update, fix for dispel test, fix for awakened state vs flock shepherd fix #759

--- test/server/cards/TheAwakenedState.spec.js
+++ test/server/cards/TheAwakenedState.spec.js
@@ -77,4 +77,46 @@ describe('Awakened State', function () {
             expect(this.sariaGuideman.damage).toBe(1);
         });
     });
+
+    describe('vs flock shepherd', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'orrick-gilstream',
+                    inPlay: ['hammer-knight'],
+                    spellboard: ['concentration', 'concentration'],
+                    dicepool: ['natural', 'natural', 'time', 'charm'],
+                    archives: ['the-awakened-state']
+                },
+                player2: {
+                    phoenixborn: 'saria-guideman',
+                    inPlay: ['flute-mage', 'ash-spirit', 'flock-shepherd']
+                }
+            });
+            this.orrickGilstream.tokens.status = 7;
+        });
+
+        it('awakened state attack action with enemy units', function () {
+            // attach card to orrick.
+            expect(this.orrickGilstream.status).toBe(7);
+            this.player1.clickCard(this.concentration);
+            this.player1.clickPrompt('Concentration');
+            this.player1.clickPrompt('main');
+            this.player1.clickDie(0);
+            this.player1.clickDie(1);
+            this.player1.clickDone();
+            this.player1.clickDone(); // choose exhausted dice
+            expect(this.orrickGilstream.upgrades.length).toBe(1);
+
+            this.player1.clickCard(this.theAwakenedState);
+            this.player1.clickPrompt('awaken');
+            expect(this.player1).not.toBeAbleToSelect(this.ashSpirit);
+            this.player1.clickCard(this.ashSpirit);
+            this.player1.clickCard(this.fluteMage);
+
+            expect(this.fluteMage.damage).toBe(1);
+            expect(this.orrickGilstream.status).toBe(7); // added one via concentration, then used one
+            expect(this.sariaGuideman.damage).toBe(1);
+        });
+    });
 });
