5a938be10b667992805c12ccffc39eca7bea59a4
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-01-07 15:55:24 +0000

    awakened state rework to avoid promptForSelect & play nicely with GV. Fix #847

--- test/server/cards/TheAwakenedState.spec.js
+++ test/server/cards/TheAwakenedState.spec.js
@@ -119,4 +119,50 @@ describe('Awakened State', function () {
             expect(this.sariaGuideman.damage).toBe(1);
         });
     });
+
+    describe('vs Golden Veil', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'orrick-gilstream',
+                    inPlay: ['hammer-knight'],
+                    spellboard: ['concentration', 'concentration'],
+                    dicepool: ['natural', 'natural', 'time', 'charm'],
+                    archives: ['the-awakened-state']
+                },
+                player2: {
+                    phoenixborn: 'saria-guideman',
+                    dicepool: ['natural', 'natural', 'charm', 'charm'],
+                    hand: ['golden-veil'],
+                    inPlay: ['flute-mage']
+                }
+            });
+            this.orrickGilstream.tokens.status = 7;
+        });
+
+        it('awakened state attack action with enemy units', function () {
+            // attach card to orrick.
+            expect(this.orrickGilstream.status).toBe(7);
+            this.player1.clickCard(this.concentration);
+            this.player1.clickPrompt('Concentration');
+            this.player1.clickPrompt('main');
+            this.player1.clickDie(0);
+            this.player1.clickDie(1);
+            this.player1.clickDone();
+            this.player1.clickDone(); // choose exhausted dice
+            expect(this.orrickGilstream.upgrades.length).toBe(1);
+
+            this.player1.clickCard(this.theAwakenedState);
+            this.player1.clickPrompt('awaken');
+            this.player1.clickCard(this.fluteMage);
+            expect(this.player2).toBeAbleToSelect(this.goldenVeil);
+            this.player2.clickCard(this.goldenVeil);
+
+            // cost
+            expect(this.orrickGilstream.status).toBe(7); // added one via concentration, then used one
+            // golden veil cancels damage to enemy and remaining ability effects
+            expect(this.fluteMage.damage).toBe(0);
+            expect(this.sariaGuideman.damage).toBe(0);
+        });
+    });
 });
