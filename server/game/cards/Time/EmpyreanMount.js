7a54f35eebb21373d510dbb09f6f04b4e6b28ad8
5H4Z <89332227+5H4Z@users.noreply.github.com>
2022-02-14 07:35:54 +1100

    Empyrean Mount WIP

--- server/game/cards/Time/EmpyreanMount.js
+++ server/game/cards/Time/EmpyreanMount.js
@@ -5,6 +5,30 @@ class EmpyreanMount extends Card {
         this.dismount();
 
         //Battlemaster
+        this.forcedReaction({
+            when: {
+                onAttackersDeclared: (event, context) => {
+                    // I'm the attacker
+                    return event.battles.some(
+                        (b) => b.attacker === context.source && this.attack.isPBAttack // Not sure about second conditional
+                    );
+                }
+            },
+            target: {
+                activePromptTitle: 'Choose a unit to force block',
+                optional: true,
+                cardType: BattlefieldTypes,
+                controller: 'opponent',
+                cardCondition: (card, context) => card !== context.source,
+                gameAction: (context) => [
+                    ability.actions.cardLastingEffect({
+                        effect: ability.effects.forceBlock(), //Yet to define where this effect takes place
+                        duration: 'untilEndOfTurn'
+                    }),
+                    this.attack.setBlockerForAttacker(context.card, context.source)
+                ]
+            }
+        });
     }
 }
 
