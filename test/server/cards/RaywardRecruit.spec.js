228363f0c0a3d47a0c08a3fa7d8975da9aa9e256
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-02-20 20:23:54 +0000

    rayward recruit with test

---
+++ test/server/cards/RaywardRecruit.spec.js
@@ -0,0 +1,32 @@
+describe('Rayward Recruit ', function () {
+    describe('Enters play effect', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'dimona-odinstar',
+                    inPlay: ['mist-spirit'],
+                    dicepool: ['divine', 'charm'],
+                    hand: ['rayward-recruit'],
+                    discard: ['hammer-knight']
+                },
+                player2: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['frost-fang', 'iron-worker'],
+                    dicepool: ['illusion', 'natural'],
+                    hand: ['particle-shield', 'safeguard']
+                }
+            });
+        });
+
+        it('Armed allows divine die buff', function () {
+            this.player1.clickCard(this.raywardRecruit);
+            this.player1.clickPrompt('Play this Ally');
+            expect(this.raywardRecruit.location).toBe('play area');
+
+            expect(this.player1).toHavePrompt('Choose a die');
+            this.player1.clickDie(0);
+            this.player1.clickCard(this.raywardRecruit);
+            expect(this.raywardRecruit.attack).toBe(1);
+        });
+    });
+});
