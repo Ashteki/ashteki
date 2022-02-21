a9a2a284b2171d230698de81955ef447f284cbb8
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-02-21 21:51:13 +0000

    reinforce first draft

---
+++ server/game/cards/Time/Reinforce.js
@@ -0,0 +1,35 @@
+const { CardType } = require('../../../constants.js');
+const Card = require('../../Card.js');
+
+class Reinforce extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            title: 'Reinforce',
+            gameAction: [
+                ability.actions.moveCard((context) => ({
+                    showMessage: true,
+                    destination: 'hand',
+                    promptWithHandlerMenu: {
+                        optional: true,
+                        activePromptTitle: 'You may place an ally or alteration into your hand',
+                        cards: context.player.deck.slice(0, 5),
+                        condition: (card) => [CardType.Ally, CardType.Upgrade].includes(card.type)
+                    }
+                })),
+                ability.actions.shuffleDeck()
+            ],
+            then: {
+                may: 'play the card you drew?',
+                condition: (context) => context.preThenEvent && context.preThenEvent.card,
+                gameAction: ability.actions.playCard((context) => ({
+                    target: context.preThenEvent.card,
+                    ignoreActionCost: true
+                }))
+            }
+        });
+    }
+}
+
+Reinforce.id = 'reinforce';
+
+module.exports = Reinforce;
