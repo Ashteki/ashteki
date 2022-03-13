0c15184582da99915809dffee82cc2db989b301d
5H4Z <89332227+5H4Z@users.noreply.github.com>
2022-03-13 20:52:39 +1100

    Revert Blood Shaman due to failed test

--- server/game/cards/Ashes-Core/BloodShaman.js
+++ server/game/cards/Ashes-Core/BloodShaman.js
@@ -15,7 +15,7 @@ class BloodShaman extends Card {
                 alwaysTriggers: true,
                 target: {
                     toSelect: 'die',
-                    dieCondition: (die) => !die.exhausted,
+                    //dieCondition: (die) => !die.exhausted,
                     owner: 'self',
                     gameAction: ability.actions.raiseDie({ showMessage: true })
                 }
