0364580cb091aca42ed1cc87485ff683df8b4be9
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-04-06 11:35:29 +0100

    fear recovery with alterations fix #951

--- test/server/cards/Fear.spec.js
+++ test/server/cards/Fear.spec.js
@@ -6,7 +6,7 @@ describe('Fear', function () {
                     phoenixborn: 'jessa-na-ni',
                     inPlay: ['enchanted-violinist', 'mist-spirit', 'blue-jaguar'],
                     dicepool: ['natural', 'illusion', 'charm', 'charm'],
-                    hand: ['fear']
+                    hand: ['fear', 'undying-heart']
                 },
                 player2: {
                     phoenixborn: 'coal-roarkwin',
@@ -28,6 +28,26 @@ describe('Fear', function () {
 
             expect(this.jessaNaNi.damage).toBe(2);
             expect(this.enchantedViolinist.location).toBe('discard');
+
+            expect(this.player1).toHavePrompt('Choose a unit to discard');
+            this.player1.clickCard(this.ironWorker); // discard iron worker
+            expect(this.ironWorker.location).toBe('discard');
+        });
+
+        it('includes alteration stat changes', function () {
+            this.player1.play(this.undyingHeart, this.enchantedViolinist); // attach to ms
+            expect(this.enchantedViolinist.recover).toBe(2);
+
+            this.player1.clickCard(this.fear); // play card
+            this.player1.clickPrompt('Play this action');
+            this.player1.clickDie(0);
+            expect(this.player1).toHavePrompt('Choose a unit to destroy');
+            this.player1.clickCard(this.enchantedViolinist); // destroy
+            this.player1.clickYes(); // return ally to hand
+            this.player1.clickPrompt('Pass'); // jessa ability pass
+
+            expect(this.jessaNaNi.damage).toBe(1);
+            expect(this.enchantedViolinist.location).toBe('hand');
 
             expect(this.player1).toHavePrompt('Choose a unit to discard');
             this.player1.clickCard(this.ironWorker); // discard iron worker
