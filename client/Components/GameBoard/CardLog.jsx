2b1ec263a19ec72b5f3a83d8a14085fa9f41ec3f
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-05-17 14:49:31 +0100

    limiting cardlog under safari, adding browser detector. close #291

--- client/Components/GameBoard/CardLog.jsx
+++ client/Components/GameBoard/CardLog.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { isSafari } from 'react-device-detect';
 import CardImage from './CardImage';
 
 import './CardZoom.scss';
@@ -59,21 +59,27 @@ const CardLog = ({ cards, onMouseOut, onMouseOver }) => {
         );
     };
 
-    const cardPics = cards.length > 1 ? cards.slice(0, cards.length - 1).map((c) => renderSimpleCard(c)) : null;
-    const firstCard = cards.length ? renderLastCard(cards[cards.length - 1]) : null;
+    let logLength = cards.length - 1;
+    if (isSafari) {
+        logLength = Math.min(3, logLength);
+    }
+
+    const cardPics =
+        cards.length > 1 ? cards.slice(0, logLength).map((c) => renderSimpleCard(c)) : null;
+    const firstCard = cards.length ? renderLastCard(cards[logLength]) : null;
     // const size = card.type === 'decklist' ? 'x-large' : 'normal';
     const arrow = show ? '︿' : '﹀';
     return (
-        <div className='cardlog-wrapper'        >
+        <div className='cardlog-wrapper'>
             {firstCard}
-            < div className='card-log bg-dark' >
+            <div className='card-log bg-dark'>
                 {cardPics}
-                < div className='card-log-arrow' onClick={() => setShow(!show)}>
+                <div className='card-log-arrow' onClick={() => setShow(!show)}>
                     {arrow}
-                </div >
-            </div >
-
-        </div>);
+                </div>
+            </div>
+        </div>
+    );
 };
 
 CardLog.displayName = 'CardLog';
