8bc45ce6817a09d37502a1eb63ec5ec431f1a0d2
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-06-23 14:43:09 +0100

    usergames

---
+++ server/scripts/playergames.js
@@ -0,0 +1,12 @@
+const monk = require('monk');
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ashteki';
+console.log('attached to: ' + mongoUrl);
+let db = monk(mongoUrl);
+console.log('selecting game: ' + process.argv[2]);
+const collection = db.get('games');
+collection
+    .find({ 'player.name': process.argv[2] })
+    .then((result) => {
+        console.log(result);
+    })
+    .then(() => db.close());
