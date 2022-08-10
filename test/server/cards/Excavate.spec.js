9197c29fe04d257d53a816abf8777d320b30f0a8
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-08-10 16:29:05 +0100

    excavate random test deck fix

--- test/server/cards/Excavate.spec.js
+++ test/server/cards/Excavate.spec.js
@@ -27,6 +27,13 @@ describe('Excavate action spell', function () {
     });
 
     it('discard 3 cards from deck then 1 to hand and deal 3 damage', function () {
+        this.player1.player.deck = [
+            this.summonButterflyMonk,
+            this.kneel,
+            this.summonSteadfastGuardian,
+            this.purge,
+            this.ironWorker
+        ];
         this.player1.play(this.excavate);
         this.player1.clickDie(0);
         this.player1.clickDone();
