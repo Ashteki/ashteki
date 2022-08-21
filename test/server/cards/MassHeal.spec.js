aaafab979977fd06286d7da0b655f4679bbf880a
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-08-21 22:44:05 +0100

    dataupdate

--- test/server/cards/MassHeal.spec.js
+++ test/server/cards/MassHeal.spec.js
@@ -24,7 +24,7 @@ describe('Mass Heal action spell', function () {
     it('heals all MY units plus pb', function () {
         this.player1.clickCard(this.massHeal);
         this.player1.clickPrompt('Play this action');
-        this.player1.clickPrompt('Remove a wound from your units and PB');
+        this.player1.clickPrompt('Heal PB + Units');
 
         expect(this.hammerKnight.damage).toBe(0);
         expect(this.silverSnake.damage).toBe(1);
@@ -35,7 +35,7 @@ describe('Mass Heal action spell', function () {
     it('heals all units (no PB) including opponents if I select the option', function () {
         this.player1.clickCard(this.massHeal);
         this.player1.clickPrompt('Play this action');
-        this.player1.clickPrompt('Remove a wound from all units');
+        this.player1.clickPrompt('Heal all units');
 
         expect(this.hammerKnight.damage).toBe(0);
         expect(this.silverSnake.damage).toBe(0);
@@ -49,7 +49,7 @@ describe('Mass Heal action spell', function () {
         this.player1.clickCard(this.massHeal);
         this.player1.clickPrompt('Play this action');
 
-        this.player1.clickPrompt('Remove a wound from all units');
+        this.player1.clickPrompt('Heal all units');
 
         expect(this.hammerKnight.damage).toBe(0);
         expect(this.silverSnake.damage).toBe(0);
