3581e1647150d7ddad95f0a64fedbcd278cac972
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2022-12-19 16:30:07 +0000

    allgames script

---
+++ server/scripts/temp/allGameDates.js
@@ -0,0 +1,27 @@
+const GameService = require('../../services/AshesGameService.js');
+const ConfigService = require('../../services/ConfigService.js');
+
+const configService = new ConfigService();
+let gameService = new GameService(configService);
+let start = new Date('2018-08-26T13:00:00');
+let end = new Date();
+console.log('Games between', start, 'and', end);
+let args = process.argv.slice(2);
+
+gameService
+    .getAllGames(start, end)
+    .then(async (games) => {
+        games.sort((a, b) => a.finishedAt < b.finishedAt);
+        console.log('datetime | dayofweek | time');
+        for (const game of games) {
+            console.log(
+                '%s | %s | %s | %s',
+                game.startedAt,
+                game.startedAt.getDay(),
+                game.startedAt.getTime()
+            );
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
