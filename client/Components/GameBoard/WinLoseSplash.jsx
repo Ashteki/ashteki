5cf5c1ec9f319ed5bc5db33a190f7b1ffea68b67
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-01-17 21:04:28 +0000

    Adding game win splash

---
+++ client/Components/GameBoard/WinLoseSplash.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { closeGameSocket, sendGameMessage } from '../../redux/actions';
+import Panel from '../Site/Panel';
+import CardImage from './CardImage';
+
+import './WinLoseSplash.scss';
+
+const WinLoseSplash = ({ game, onCloseClick }) => {
+    const dispatch = useDispatch();
+
+    const onLeaveClick = () => {
+        dispatch(sendGameMessage('leavegame'));
+        dispatch(closeGameSocket());
+    };
+
+    let loser = null;
+    let winner = null;
+
+    for (const player in game.players) {
+        const p = game.players[player];
+        if (p.name === game.winner) {
+            winner = p;
+        } else {
+            loser = p;
+        }
+    }
+
+    const headerMessage = winner?.name + ' ' + 'wins!';
+    const card = winner?.phoenixborn;
+    // const winner = game.players[game.winner];
+    return (
+        <Panel title='Game Over' cardClass={`splash`} onCloseClick={onCloseClick}>
+            <CardImage card={card} />
+            <div className='central'>
+                <h2>{headerMessage}</h2>
+                <div className='buttonDiv'>
+                    <button
+                        onClick={onLeaveClick}
+                        className='btn prompt-button btn-default'
+                    >Leave</button>
+                </div>
+            </div>
+            {/* <CardImage card={loser.phoenixborn} imgClass='splash-loser' /> */}
+        </Panel>
+    );
+};
+
+export default WinLoseSplash;
