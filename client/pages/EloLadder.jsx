50f77f43150103358d2a4adf5536f6821c685047
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-01-23 17:39:35 +0000

    script to update game count (ranked)

--- client/pages/EloLadder.jsx
+++ client/pages/EloLadder.jsx
@@ -24,7 +24,7 @@ const EloLadder = () => {
                 <h2>Current Rankings for Active Players</h2>
                 <p>These rankings are an elo-derived value determined by wins/losses for games marked as 'Ranked' in the game setup screen. The ranking is intended as a relative indicator of skill, and not an absolute ordering of players. We don't have the volume of games to make these figures super-precise, but they should give you an indication of the kind of player you are about to meet.</p>
                 <p>Players who do not play a ranked game (are 'inactive') for over a month will not appear on this list, but will not lose their rating, or have it reduced arbitrarily. Inactive players may pick up where they left off as soon as they play a ranked game, and will again appear on this page.</p>
-                <p>New players begin with a score of 1500, but will typically dip below that value while learning the ropes.</p>
+                <p>New players begin with a score of 1500, but will typically dip below that value while learning the ropes. Players need 12 ranked games before they will appear on this list.</p>
                 <table className='elo-ladder'>
                     {output}
                 </table>
---
+++ server/scripts/gameCountUpdate.js
@@ -0,0 +1,64 @@
+const _ = require('underscore');
+const GameService = require('../services/AshesGameService.js');
+const ConfigService = require('../services/ConfigService.js');
+const monk = require('monk');
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ashteki';
+
+let db = monk(mongoUrl);
+console.log('attached to: ' + mongoUrl);
+
+let gameService = new GameService(new ConfigService());
+/*
+let args = process.argv.slice(2);
+
+if(_.size(args) < 2) {
+    console.error('Must provide start and end date');
+
+    db.close();
+    return;
+}
+*/
+
+let start = new Date('2018-08-26T13:00:00');
+let end = new Date();
+//console.info('Running stats between', args[0], 'and', args[1]);
+// console.info('Games between', start, 'and', end);
+
+gameService.getAllGames(start, end)
+    .then((games) => {
+        const rankedGames = games.filter(g => g.gameType === 'competitive');
+        console.log('ranked game count: ' + rankedGames.length);
+        let players = {};
+        _.each(rankedGames, (game) => {
+            if (_.size(game.players) !== 2) {
+                return;
+            }
+
+            _.each(game.players, (player) => {
+                if (!players[player.name]) {
+                    players[player.name] = { name: player.name, played: 0 };
+                }
+
+                var playerStat = players[player.name];
+                playerStat.played++;
+            });
+        });
+
+        console.log('users');
+        const collection = db.get('users');
+        console.log(players);
+        _.each(players, (player) => {
+            console.log(player);
+            collection
+                .update(
+                    { username: player.name },
+                    {
+                        $set: {
+                            rankedGamesPlayed: player.played
+                        }
+                    }
+                )
+        })
+
+
+    });
