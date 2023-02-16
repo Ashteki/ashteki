2e28c9e8aadd9cf236f1399f9fa9ccff8959f301
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-02-16 19:55:36 +0000

    dummy can't choose between sources

--- server/game/cards/Chimera/Rampage.js
+++ server/game/cards/Chimera/Rampage.js
@@ -1,18 +1,30 @@
+const { Level } = require("../../../constants");
 const RevealAct = require("../../BaseActions/RevealAct");
 const Card = require("../../Card");
 const BehaviourCard = require("../../solo/BehaviourCard");
 
 class Rampage extends Card {
-    // setupCardAbilities(ability) {
-    //     this.entersPlay({
-    //         gameAction: ability.actions.addStatusToken((context) => ({
-    //             amount: 2,
-    //             target: context.source
-    //         }))
-    //     });
-    // }
+    setupCardAbilities(ability) {
+        this.forcedReaction({
+            when: {
+                // it's my turn
+                onBeginTurn: (event, context) => event.player === context.player
+            },
+            location: 'play area',
+            gameAction: ability.actions.removeStatus({ target: this }),
+            then: {
+                target: {
+                    toSelect: 'die',
+                    autoTarget: (context) => context.player.dice.filter(d => d.level === Level.Basic),
+                    gameAction: ability.actions.rerollDice()
+                }
+            }
+        });
+    }
 
-    get statusCount() { return 2 }
+    get statusCount() {
+        return 2;
+    }
 }
 
 Rampage.id = 'rampage';
