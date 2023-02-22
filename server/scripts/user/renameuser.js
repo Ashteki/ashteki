518db1eeeaed8a64d4ba2a5e2bb9ff22fc1be694
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-02-22 13:10:04 +0000

    rename user script update

--- server/scripts/user/renameuser.js
+++ server/scripts/user/renameuser.js
@@ -2,12 +2,14 @@ const monk = require('monk');
 const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ashteki';
 console.log('attached to: ' + mongoUrl);
 if (process.argv[3] !== '') {
+    const oldName = process.argv[2];
+    const newName = process.argv[3];
     let db = monk(mongoUrl);
-    console.log('updating username: ' + process.argv[2] + ' to: ' + process.argv[3]);
+    console.log('updating username: ' + oldName + ' to: ' + newName);
     const collection = db.get('users');
     collection
         .update(
-            { username: process.argv[2] },
+            { username: oldName },
             {
                 $set: {
                     username: process.argv[3]
@@ -17,6 +19,33 @@ if (process.argv[3] !== '') {
         .then((result) => {
             // Updated the document with the field a equal to 2
             console.log(result);
+        });
+    // .then(() => db.close());
+
+    const colGames = db.get('games');
+    colGames
+        .update(
+            { 'players.name': oldName },
+            { $set: { 'players.$.name': newName } },
+            { multi: true }
+        )
+        .then((result) => {
+            // Updated the document with the field a equal to 2
+            console.log(result);
+        });
+    // .then(() => db.close());
+
+    const colGames2 = db.get('games');
+    colGames2
+        .update(
+            { winner: oldName },
+            { $set: { winner: newName } },
+            { multi: true }
+        )
+        .then((result) => {
+            // Updated the document with the field a equal to 2
+            console.log(result);
         })
         .then(() => db.close());
+
 }
