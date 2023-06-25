5764f72324f018dfc7cf636c64fd0e0907a66aef
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-06-25 13:18:37 +0100

    newstarters script change to show conversions

--- server/scripts/user/newstarters.js
+++ server/scripts/user/newstarters.js
@@ -1,32 +1,62 @@
 const _ = require('underscore');
 const UserService = require('../../services/AshesUserService.js');
 const ConfigService = require('../../services/ConfigService.js');
+const GameService = require('../../services/AshesGameService.js');
 
-let userService = new UserService(new ConfigService());
+const configService = new ConfigService();
+let start = new Date('2018-08-26T13:00:00');
+let end = new Date();
+const gameService = new GameService(configService);
 
-userService
-    .getAllUsers()
-    .then((users) => {
-        console.info('' + _.size(users), 'total users');
-
-        const starterStats = {};
-        _.each(users, (user) => {
-            const joinMonth =
-                user.registered.getFullYear() +
-                '-' +
-                ('0' + (user.registered.getMonth() + 1)).slice(-2);
-            if (starterStats[joinMonth]) {
-                starterStats[joinMonth]++;
-            } else {
-                starterStats[joinMonth] = 1;
-            }
-        });
-
-        for (let j in starterStats) {
-            console.log(`${j} | ${starterStats[j]}`);
+gameService.getAllGames(start, end).then((games) => {
+    const userGameCount = games.reduce(function (agg, game) {
+        if (game.players.length < 2) {
+            console.log('exiting');
+            return agg;
         }
-        console.info('' + _.size(users), 'total users');
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+        const p1Name = game.players[0].name;
+        const p2Name = game.players[1].name;
+
+        agg[p1Name] = agg[p1Name] || 0;
+        agg[p2Name] = agg[p2Name] || 0;
+
+        agg[p1Name]++;
+        agg[p2Name]++;
+        return agg;
+    }, {});
+
+    const starterList = {};
+
+    let userService = new UserService(configService);
+    userService
+        .getAllUsers()
+        .then((users) => {
+            console.info('' + _.size(users), 'total users');
+
+            _.each(users, (user) => {
+                const joinMonth =
+                    user.registered.getFullYear() +
+                    '-' +
+                    ('0' + (user.registered.getMonth() + 1)).slice(-2);
+
+                if (!starterList[joinMonth]) {
+                    starterList[joinMonth] = [];
+                }
+                starterList[joinMonth].push({
+                    username: user.username,
+                    count: userGameCount[user.username] || 0
+                });
+            });
+
+            for (let j in starterList) {
+                console.log(
+                    `${j} | ${starterList[j].length} | ${starterList[j].filter((s) => s.count > 0).length
+                    }`
+                );
+            }
+            console.info('' + _.size(users), 'total users');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
