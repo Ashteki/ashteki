c55fdee63a194479039b74fe9db74f2cba116047
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-07-14 08:00:17 +0100

    taggedGames api

--- server/api/games.js
+++ server/api/games.js
@@ -3,8 +3,11 @@ const passport = require('passport');
 const GameService = require('../services/AshesGameService.js');
 const ConfigService = require('../services/ConfigService.js');
 const { wrapAsync } = require('../util.js');
+const AshesDeckService = require('../services/AshesDeckService.js');
 
-let gameService = new GameService(new ConfigService());
+const configService = new ConfigService();
+const gameService = new GameService(configService);
+const deckService = new AshesDeckService(configService);
 
 module.exports.init = function (server) {
     server.get(
@@ -15,4 +18,56 @@ module.exports.init = function (server) {
             res.send({ success: true, games: games });
         })
     );
+
+    server.get(
+        '/api/games/:tag',
+        passport.authenticate('jwt', { session: false }),
+        wrapAsync(async function (req, res) {
+            let games = await gameService.getTaggedGames(req.params.tag);
+            const results = await Promise.all(
+                games.map(async (game) => {
+                    const gameResult = await getGameResult(game);
+                    return gameResult;
+                })
+            );
+
+            res.send({ success: true, results: results });
+        })
+    );
 };
+
+async function getPlayer(game, index) {
+    const player = game.players[index];
+    if (player.deckid) {
+        const deck = await deckService.getById(player.deckid);
+        const deckAudit = deck
+            ? {
+                deckId: player.deckid,
+                ashesLiveModified: deck.ashesLiveModified,
+                created: deck.created,
+                lastUpdated: deck.lastUpdated,
+                checkMe: deck.lastUpdated > deck.created
+            } : null;
+
+        // console.log(deckAudit);
+        player.deckAudit = deckAudit;
+    }
+    return player;
+}
+
+async function getGameResult(game) {
+
+    const p1 = await getPlayer(game, 0);
+    const p2 = await getPlayer(game, 1);
+    const result = {
+        label: game.label,
+        id: game.id,
+        startedAt: game.startedAt.toUTCString(),
+        winner: game.winner,
+
+        p1: p1,
+        p2: p2
+    };
+    // console.log("gameResult: ", result);
+    return result;
+}
