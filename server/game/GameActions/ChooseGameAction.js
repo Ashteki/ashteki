14203ba35055f2e1b8b343977b143fa4e4eb0eda
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-08-22 14:50:36 +0100

    frost frog

--- server/game/GameActions/ChooseGameAction.js
+++ server/game/GameActions/ChooseGameAction.js
@@ -15,7 +15,7 @@ class ChooseGameAction extends GameAction {
 
     setup() {
         super.setup();
-        this.effectMsg = 'choose between different actions';
+        this.effectMsg = 'take a chosen action';
     }
 
     update(context) {
---
+++ server/game/cards/Frostwild/FrostFrog.js
@@ -0,0 +1,35 @@
+const Card = require('../../Card.js');
+
+class FrostFrog extends Card {
+    setupCardAbilities(ability) {
+        this.forcedReaction({
+            when: {
+                onAttackersDeclared: (event, context) => {
+                    return (
+                        event.attackingPlayer === context.source.controller &&
+                        context.source.isAttacker
+                    );
+                }
+            },
+            gameAction: ability.actions.chooseAction((context) => ({
+                choices: {
+                    '+1 Attack': ability.actions.cardLastingEffect(() => ({
+                        target: this,
+                        effect: ability.effects.modifyAttack(1),
+                        duration: 'untilEndOfTurn'
+                    })),
+                    Exhaust: ability.actions.exhaust({
+                        promptForSelect: {
+                            cardCondition: (card) => card.life <= this.getAbilityNumeric(1),
+                            controller: 'opponent'
+                        }
+                    })
+                }
+            }))
+        });
+    }
+}
+
+FrostFrog.id = 'frost-frog';
+
+module.exports = FrostFrog;
