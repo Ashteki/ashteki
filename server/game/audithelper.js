6d842c1f669d294acd57c2daa4ed8fce94eefb66
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-12-12 09:53:40 +0000

    fix #1475: audithelper error if game not started

--- server/game/audithelper.js
+++ server/game/audithelper.js
@@ -8,24 +8,27 @@ class AuditHelper {
     snapshotState(type) {
         const activePlayer = this.game.activePlayer;
 
-        this.audit.push({
-            type: type,
-            round: this.game.round,
-            activePlayer: activePlayer.username,
-            playerTurn: activePlayer.turn,
-            playerState: this.game.getPlayers().map((player) => ({
-                turn: player.turn,
-                wounds: player.phoenixborn.damage,
-                unitCount: player.unitsInPlay.length,
-                diceUnspent: player.dice.filter((d) => !d.exhausted).length,
-                cardsPlayed: this.game.gameLog.filter(
-                    (l) => l.player === player && l.act === 'play'
-                ).length,
-                medCount: player.medCount,
-                deck: player.deck.length,
-                hand: player.hand.length
-            }))
-        });
+        // e.g. no activePlayer if the game hasn't started past FF
+        if (activePlayer) {
+            this.audit.push({
+                type: type,
+                round: this.game.round,
+                activePlayer: activePlayer.username,
+                playerTurn: activePlayer.turn,
+                playerState: this.game.getPlayers().map((player) => ({
+                    turn: player.turn,
+                    wounds: player.phoenixborn.damage,
+                    unitCount: player.unitsInPlay.length,
+                    diceUnspent: player.dice.filter((d) => !d.exhausted).length,
+                    cardsPlayed: this.game.gameLog.filter(
+                        (l) => l.player === player && l.act === 'play'
+                    ).length,
+                    medCount: player.medCount,
+                    deck: player.deck.length,
+                    hand: player.hand.length
+                }))
+            });
+        }
     }
 
     getReport() {
