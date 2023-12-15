a0933fc9f0b8267c992cd135f0c2d9fd5c94edbf
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2023-12-15 23:31:14 +0000

    gifted rose reject. discard or die cost

--- server/game/cards/Neverset/GiftedRose.js
+++ server/game/cards/Neverset/GiftedRose.js
@@ -6,6 +6,15 @@ class GiftedRose extends Card {
         this.whileAttached({
             effect: [ability.effects.cardCannot('guard')]
         });
+
+        this.action({
+            title: 'Reject',
+            cost: [
+                ability.costs.sideAction(),
+                ability.costs.chosenDieOrDiscard()
+            ],
+            gameAction: ability.actions.discard({ target: this })
+        })
     }
 
     canAttach(card, context) {
--- test/server/cards/GiftedRose.spec.js
+++ test/server/cards/GiftedRose.spec.js
@@ -17,7 +17,7 @@ describe('Gifted Rose', function () {
             });
         });
 
-        it('attach. cannot guard', function () {
+        it('attach. cannot guard. Reject with die', function () {
             this.player1.play(this.giftedRose);
             this.player1.clickDie(0);
 
@@ -30,6 +30,37 @@ describe('Gifted Rose', function () {
             this.player2.clickNo();
 
             expect(this.ironRhino.damage).toBe(1);
+            this.player1.endTurn();
+
+            this.player2.clickCard(this.giftedRose);
+            this.player2.clickPrompt('Reject');
+            this.player2.clickPrompt('Basic Die');
+            this.player2.clickDie(0);
+            expect(this.giftedRose.location).toBe('discard');
+            expect(this.player2.dicepool[0].exhausted).toBe(true);
+        });
+
+        it('attach. cannot guard. Reject with discard', function () {
+            this.player1.play(this.giftedRose);
+            this.player1.clickDie(0);
+
+            expect(this.aradelSummergaard.upgrades.length).toBe(1);
+
+            this.player1.clickAttack(this.ironRhino);
+            this.player1.clickCard(this.mistSpirit);
+
+            // no guard, only counter
+            this.player2.clickNo();
+
+            expect(this.ironRhino.damage).toBe(1);
+            this.player1.endTurn();
+
+            this.player2.clickCard(this.giftedRose);
+            this.player2.clickPrompt('Reject');
+            this.player2.clickPrompt('Discard');
+            this.player2.clickCard(this.choke);
+            expect(this.giftedRose.location).toBe('discard');
+            expect(this.choke.location).toBe('discard');
         });
     });
 });
