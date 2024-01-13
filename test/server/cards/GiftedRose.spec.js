578004c6a0e8856678a04dcc9e40e4bfdb374ee9
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-01-13 11:01:50 +0000

    fix gifted rose bug report when hand is empty

--- test/server/cards/GiftedRose.spec.js
+++ test/server/cards/GiftedRose.spec.js
@@ -66,6 +66,48 @@ describe('Gifted Rose', function () {
         });
     });
 
+    describe('opponent has no hand', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'maeoni-viper',
+                    inPlay: ['mist-spirit', 'raptor-herder', 'time-hopper'],
+                    hand: ['gifted-rose'],
+                    dicepool: ['ceremonial', 'charm']
+                },
+                player2: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['hammer-knight', 'iron-rhino', 'adept-duelist'],
+                    dicepool: ['ceremonial', 'charm'],
+                    hand: []
+                }
+            });
+        });
+
+        it('bug report', function () {
+            this.player1.play(this.giftedRose);
+            this.player1.clickDie(0);
+
+            expect(this.aradelSummergaard.upgrades.length).toBe(1);
+
+            this.player1.clickAttack(this.ironRhino);
+            this.player1.clickCard(this.mistSpirit);
+
+            // no guard, only counter
+            this.player2.clickNo();
+
+            expect(this.ironRhino.damage).toBe(1);
+            this.player1.endTurn();
+
+            this.player2.clickCard(this.giftedRose);
+            this.player2.clickPrompt('Reject');
+            // this.player2.clickPrompt('Basic Die'); // no prompt to choose as no choice
+            this.player2.clickDie(0);
+            expect(this.giftedRose.location).toBe('discard');
+            expect(this.player2.dicepool[0].exhausted).toBe(true);
+        });
+    });
+
     describe('attached to chimera', function () {
         beforeEach(function () {
             this.setupTest({
