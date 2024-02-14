394aab454d72592f388e8ca02d09bfc7b6dd62ff
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-02-14 15:54:27 +0000

    fix #1523: backtrack to use getHand() - fix vs chimera

--- test/server/cards/Backtrack.spec.js
+++ test/server/cards/Backtrack.spec.js
@@ -58,4 +58,43 @@ describe('Backtrack', function () {
         });
     });
 
+    describe('vs chimera aspects', function () {
+        beforeEach(function () {
+            this.setupTest({
+                mode: 'solo',
+                player1: {
+                    phoenixborn: 'lulu-firststone',
+                    inPlay: ['hammer-knight', 'anchornaut', 'mist-spirit', 'string-mage'],
+                    dicepool: ['ceremonial', 'time', 'charm', 'charm'],
+                    hand: ['iron-worker', 'backtrack', 'purge']
+                },
+                player2: {
+                    dummy: true,
+                    phoenixborn: 'corpse-of-viros',
+                    behaviour: 'viros-behaviour',
+                    ultimate: 'viros-ultimate',
+                    inPlay: ['hunting-instincts', 'rampage'],
+                    deck: [],
+                    dicepool: ['basic', 'basic', 'basic', 'basic', 'basic']
+                }
+            });
+        });
+
+        it('return single attacker to hand', function () {
+            this.player1.endTurn();
+            // chimera should attack with hunting instincts
+
+            this.player1.clickCard(this.backtrack);
+            this.player1.clickCard(this.purge); // discard
+
+            // return to hand (draw pile for chimera once resolved)
+            this.player1.clickCard(this.huntingInstincts);
+
+            expect(this.backtrack.location).toBe('discard');
+            expect(this.purge.location).toBe('discard');
+            expect(this.huntingInstincts.location).toBe('deck');
+            expect(this.huntingInstincts.isAttacker).toBe(false);
+            expect(this.player2.hand.length).toBe(0);
+        });
+    });
 });
