aa226c02b1cb66f7007efa5a8e1439adb8de2d75
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-02-22 22:30:07 +0000

    horrific gaze

---
+++ server/game/cards/Chimera/Neverset/HorrificGaze.js
@@ -0,0 +1,19 @@
+const { CardType } = require('../../../../constants');
+const AspectCard = require('../../../solo/AspectCard');
+
+class HorrificGaze extends AspectCard {
+    setupCardAbilities(ability) {
+        super.setupCardAbilities(ability);
+
+        this.persistentEffect({
+            condition: () => !this.exhausted,
+            match: (card) => card.type === CardType.Phoenixborn,
+            targetController: 'opponent',
+            effect: ability.effects.exhausted()
+        });
+    }
+}
+
+HorrificGaze.id = 'horrific-gaze';
+
+module.exports = HorrificGaze;
