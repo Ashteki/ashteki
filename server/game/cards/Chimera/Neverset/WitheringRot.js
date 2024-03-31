c0983f5f9c9fe09bf09e2ce0383e547df160945a
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-03-31 10:37:14 +0100

    withering rot pb damage choice

--- server/game/cards/Chimera/Neverset/WitheringRot.js
+++ server/game/cards/Chimera/Neverset/WitheringRot.js
@@ -7,12 +7,33 @@ class WitheringRot extends AspectCard {
         this.statusAbility({
             target: {
                 toSelect: 'player',
+                ignoreTargetCheck: true,
                 autoTarget: (context) => context.player.opponent,
                 gameAction: ability.actions.conditional({
                     condition: (context) => context.source.status === 0,
-                    trueGameAction: ability.actions.chosenDiscard({
+                    trueGameAction: ability.actions.chooseAction((context) => ({
+                        target: context.target,
+                        player: context.target,
+                        choices: {
+                            Discard: ability.actions.chosenDiscard(),
 
-                    })
+                            // ability.actions.conditional({
+                            //     condition: (context) => context.target.controller.getHand().length > 0,
+                            //     trueGameAction: ability.actions.discardAtRandom({
+                            //         target: context.target.controller
+                            //     }),
+                            //     falseGameAction: ability.actions.addDamageToken({
+                            //         amount: 2,
+                            //         showMessage: true
+                            //     })
+                            // }),
+                            '1 Pb Damage': ability.actions.dealDamage((context) => ({
+                                target: context.player.opponent.phoenixborn,
+                                amount: 1,
+                                showMessage: true
+                            }))
+                        }
+                    }))
                 })
             }
         });
