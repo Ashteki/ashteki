d952575d9e9064e4c9eceefe0a21180bcae003d8
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-05-16 08:36:42 +0100

    Removed Elo

--- client/pages/Results.jsx
+++ client/pages/Results.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import PictureButton from '../Components/Lobby/PictureButton';
 import './Results.scss';
-import EloLadder from './EloLadder';
 import MyResults from './MyResults';
 import UserAlts from './profile/UserAlts';
 
@@ -17,9 +16,6 @@ const Results = () => {
                     <Tab>
                         <PictureButton text='Alt Arts' imageClass='alts-link' />
                     </Tab>
-                    <Tab>
-                        <PictureButton text='Elo Standings' imageClass='elo-link' />
-                    </Tab>
                 </TabList>
                 <TabPanel>
                     <MyResults />
@@ -27,11 +23,8 @@ const Results = () => {
                 <TabPanel>
                     <UserAlts />
                 </TabPanel>
-                <TabPanel>
-                    <EloLadder key='elo' />
-                </TabPanel>
             </Tabs>
-        </div >
+        </div>
     )
 };
 
