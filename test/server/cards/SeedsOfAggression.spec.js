6abdd03b8cb30a707c1f2ffdcf513a393e05281c
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-05-19 09:24:20 +0100

    fix #1561: setting seeds Of Aggression damageSource for damageDealt

--- test/server/cards/SeedsOfAggression.spec.js
+++ test/server/cards/SeedsOfAggression.spec.js
@@ -3,13 +3,13 @@ describe('Seeds of Aggression', function () {
         this.setupTest({
             player1: {
                 phoenixborn: 'aradel-summergaard',
-                inPlay: ['iron-worker', 'flute-mage'],
+                inPlay: ['iron-worker', 'flute-mage', 'silver-paladin'],
                 dicepool: ['charm', 'charm', 'divine'],
                 hand: ['seeds-of-aggression', 'pride']
             },
             player2: {
                 phoenixborn: 'coal-roarkwin',
-                inPlay: ['hammer-knight', 'gilder'],
+                inPlay: ['hammer-knight', 'gilder', 'mist-spirit'],
                 spellboard: []
             }
         });
@@ -23,5 +23,24 @@ describe('Seeds of Aggression', function () {
 
         expect(this.hammerKnight.damage).toBe(1);
         expect(this.fluteMage.location).toBe('discard');
+    });
+
+    it('silver paladin should trigger exalt', function () {
+        this.player1.dicepool[2].exhausted = true;
+
+        this.player1.play(this.seedsOfAggression);
+        this.player1.clickCard(this.silverPaladin);
+
+        this.player1.clickCard(this.mistSpirit);
+
+        expect(this.silverPaladin.damage).toBe(1);
+
+        expect(this.player1).not.toHaveDefaultPrompt(); // should trigger exalt
+
+        this.player1.clickDie(2);
+        this.player1.clickCard(this.ironWorker);
+        expect(this.ironWorker.attack).toBe(3);
+        expect(this.player1).toHaveDefaultPrompt();
+        expect(this.mistSpirit.location).toBe('archives');
     });
 });
