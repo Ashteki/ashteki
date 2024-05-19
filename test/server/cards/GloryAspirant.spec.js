889a3ad5addd44fbd124c1d1f6047204944842c9
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-05-19 09:08:58 +0100

    fix #1562 glory aspirant lionize with no lions

--- test/server/cards/GloryAspirant.spec.js
+++ test/server/cards/GloryAspirant.spec.js
@@ -44,4 +44,40 @@ describe('Glory Aspirant', function () {
             expect(this.player1.dicepool[0].level).toBe('power');
         });
     });
+
+    describe('Enters play effect with no lions', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'dimona-odinstar',
+                    inPlay: ['mist-spirit'],
+                    dicepool: ['divine', 'charm', 'ceremonial'],
+                    hand: ['glory-aspirant'],
+                    discard: ['radiant-light', 'anchornaut'],
+                    deck: ['purge', 'chant-of-sacrifice']
+                },
+                player2: {
+                    phoenixborn: 'aradel-summergaard',
+                    inPlay: ['frost-fang', 'iron-worker'],
+                    dicepool: ['illusion', 'natural'],
+                    hand: ['particle-shield', 'safeguard']
+                }
+            });
+        });
+
+        it('no card to tutor still triggers die change', function () {
+            this.player1.dicepool[0].level = 'basic';
+            expect(this.player1.dicepool[0].level).toBe('basic');
+
+            this.player1.clickCard(this.gloryAspirant);
+            this.player1.clickPrompt('Play this Ally');
+            this.player1.clickDie(2);
+            expect(this.gloryAspirant.location).toBe('play area');
+            expect(this.player1).not.toBeAbleToSelect(this.chantOfSacrifice);
+
+            this.player1.clickDie(0);
+            this.player1.clickDone();
+            expect(this.player1.dicepool[0].level).toBe('power');
+        });
+    });
 });
