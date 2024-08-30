14979c1d52aee784d58fffefca10cbbeb1253797
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-08-30 16:33:41 +0100

    rimea 2.0 new ability

--- server/game/cards/Mixed-Expansions/RimeaCareworn.js
+++ server/game/cards/Mixed-Expansions/RimeaCareworn.js
@@ -6,37 +6,15 @@ class RimeaCareworn extends Card {
     setupCardAbilities(ability) {
         this.action({
             title: 'Visions',
-            cost: ability.costs.sideAction(),
-            target: {
-                mode: 'select',
-                activePromptTitle: "Visions: Choose a player's deck",
-                choices: [
-                    { text: 'Mine', value: false },
-                    { text: "Opponent's", value: true }
-                ],
-                choiceHandler: (option) => (this.chosenValue = option.value)
-            },
-            gameAction: ability.actions.rearrangeCards((context) => ({
-                target: this.chosenValue ? context.player.opponent : context.player,
-                purge: 0
-            })),
+            cost: [ability.costs.sideAction(), ability.costs.exhaust()],
+            gameAction: ability.actions.draw({ amount: 2 }),
             then: {
-                cost: [
-                    ability.costs.dice([new DiceCount(1, Level.Basic)],
-                        'Move top of deck to the bottom?'
-                    )],
-                gameAction: [
-                    ability.actions.moveToBottom((context) => ({
-                        target: this.chosenValue
-                            ? context.player.opponent.deck[0]
-                            : context.player.deck[0]
-                    })),
-                    ability.actions.exhaust({ showMessage: true })
-                ],
-                message: '{0} moves 1 card from the top of their deck to the bottom',
-                messageArgs: (context) => [context.player]
-            },
-            preferActionPromptMessage: true
+                target: {
+                    toSelect: 'player',
+                    autoTarget: (context) => context.player.opponent,
+                    gameAction: ability.actions.exposeRandom({ promptTitle: 'Visions', amount: 2 })
+                }
+            }
         });
     }
 }
