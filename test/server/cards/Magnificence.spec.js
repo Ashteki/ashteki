6cc87da05246af7338aed5e5e5a632eed67048c7
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2024-09-23 20:44:37 +0100

    magnificence fix

--- test/server/cards/Magnificence.spec.js
+++ test/server/cards/Magnificence.spec.js
@@ -1,3 +1,5 @@
+const Dice = require('../../../server/game/dice');
+
 describe('Magnificence', function () {
     beforeEach(function () {
         this.setupTest({
@@ -41,6 +43,23 @@ describe('Magnificence', function () {
         this.player1.clickCard(this.hammerKnight);
         this.player1.clickCard(this.hammerKnight);
         expect(this.hammerKnight.damage).toBe(2);
+        expect(this.player1).toHaveDefaultPrompt();
+    });
+
+    it('can be played to reroll MY OWN dice, invert and damage opponent', function () {
+        spyOn(Dice, 'getRandomInt').and.returnValue(4); // basic
+
+        this.player1.play(this.magnificence);
+        this.player1.clickDie(0);
+        this.player1.clickDie(1);
+        expect(this.player1).not.toBeAbleToSelectDie(this.player2.dicepool[0]);
+        this.player1.clickDie(2);
+        this.player1.clickPrompt('Done');
+        this.player1.clickCard(this.fluteMage);
+        this.player1.clickCard(this.hammerKnight);
+        this.player1.clickCard(this.hammerKnight);
+        expect(this.hammerKnight.damage).toBe(2);
+        expect(this.player1.dicepool[0].level).toBe('basic');
         expect(this.player1).toHaveDefaultPrompt();
     });
 });
