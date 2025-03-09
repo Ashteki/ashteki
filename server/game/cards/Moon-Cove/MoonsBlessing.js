15db70b0f0cae9b76e16fb5e65a0640408499e87
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-03-09 16:50:28 +0000

    Moons blessing

---
+++ server/game/cards/Moon-Cove/MoonsBlessing.js
@@ -0,0 +1,33 @@
+const { CardType } = require('../../../constants.js');
+const Card = require('../../Card.js');
+
+class MoonsBlessing extends Card {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: [ability.effects.modifyLife(1), ability.effects.modifyRecover(1)]
+        });
+
+        this.forcedReaction({
+            when: {
+                onCardAttached: (event, context) =>
+                    event.card === context.source && event.parent.controller === context.player
+            },
+            autoResolve: true,
+            target: {
+                activePromptTitle: 'Choose a ready spell to place in your hand',
+                optional: true,
+                owner: 'self',
+                location: 'discard',
+                cardType: CardType.ReadySpell,
+                gameAction: ability.actions.returnToHand({
+                    location: 'discard',
+                    showMessage: true
+                })
+            }
+        });
+    }
+}
+
+MoonsBlessing.id = 'moons-blessing';
+
+module.exports = MoonsBlessing;
