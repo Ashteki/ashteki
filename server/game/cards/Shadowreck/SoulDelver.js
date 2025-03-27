a8b2257bdc9e29b72a29b6bbb5d469c9e1651447
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-03-27 06:52:47 +0000

    fix #1642: Soul Delver multi-token

--- server/game/cards/Shadowreck/SoulDelver.js
+++ server/game/cards/Shadowreck/SoulDelver.js
@@ -1,4 +1,3 @@
-const { Level } = require('../../../constants.js');
 const Card = require('../../Card.js');
 
 class SoulDelver extends Card {
@@ -9,23 +8,13 @@ class SoulDelver extends Card {
                     return event.attackers.includes(context.source);
                 }
             },
-            // target: {
-            //     targetsPlayer: true,
-            //     toSelect: 'die',
-            //     mode: 'upTo',
-            //     dieCondition: (die) => !die.exhausted && die.level !== Level.Basic,
-            //     owner: 'opponent',
-            //     gameAction: ability.actions.changeDice((context) => ({
-            //         numDice: context.player.phoenixborn.status
-
-            //     }))
-            // }
             target: {
                 toSelect: 'player',
                 activePromptTitle: "Choose which player's dice pool to affect",
                 gameAction: ability.actions.changeDice((context) => ({
                     owner: context.target === context.player ? 'self' : 'opponent',
-                    singleLevel: true
+                    singleLevel: true,
+                    numDice: context.player.phoenixborn.status
                 }))
             }
         });
