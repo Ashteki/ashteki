fb73755fc89d75698e74c974d193b7e1e148be2d
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-04-18 12:41:55 +0100

    test tidyup / removal after mg errata

--- test/server/cards/MassiveGrowth.spec.js
+++ test/server/cards/MassiveGrowth.spec.js
@@ -46,27 +46,4 @@ describe('Massive Growth alteration', function () {
 
         expect(this.coalRoarkwin.damage).toBe(5);
     });
-
-    it('does not prevent sympathy pain targetting the unit', function () {
-        this.player1.clickCard(this.massiveGrowth);
-        this.player1.clickPrompt('Play this Alteration');
-        this.player1.clickDie(2);
-        this.player1.clickPrompt('Done');
-        this.player1.clickCard(this.mistSpirit);
-        expect(this.mistSpirit.upgrades.length).toBe(1);
-
-        expect(this.mistSpirit.attack).toBe(5);
-        expect(this.mistSpirit.life).toBe(5);
-
-        this.player1.endTurn();
-        this.player2.endTurn();
-
-        this.player1.play(this.moltenGold, this.coalRoarkwin);
-        this.player2.clickCard(this.sympathyPain);
-        this.player2.clickDie(0);
-
-        expect(this.player2).toBeAbleToSelect(this.mistSpirit);
-        this.player2.clickCard(this.mistSpirit);
-        expect(this.mistSpirit.damage).toBe(2);
-    });
 });
