b6c6f1a72fa4eabd38ba90f28c353f85dc9ccd06
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-05-26 10:52:00 +0100

    show alert for status abilities

--- server/game/gamesteps/abilityresolver.js
+++ server/game/gamesteps/abilityresolver.js
@@ -1,4 +1,5 @@
 const { Location, CardType, ActionSpellTypes } = require('../../constants.js');
+// const TriggeredAbility = require('../triggeredability.js');
 const BaseStepWithPipeline = require('./basestepwithpipeline.js');
 const SimpleStep = require('./simplestep.js');
 
@@ -180,6 +181,17 @@ class AbilityResolver extends BaseStepWithPipeline {
             this.context.ability.logUse(this.context)
         ) {
             this.game.cardUsed(this.context.source, this.context.player);
+            this.game.queueUserAlert(this.context, {
+                timed: true,
+                promptTitle: 'Aspect reaction',
+                controls: [
+                    {
+                        type: 'targeting',
+                        source: this.context.source.getShortSummary()
+                    }
+                ],
+                menuTitle: this.context.player.name + ' uses ' + this.context.source.name
+            });
         }
 
         if (
@@ -199,6 +211,7 @@ class AbilityResolver extends BaseStepWithPipeline {
                 menuTitle: this.context.player.name + ' plays ' + this.context.source.name
             });
         }
+
 
         this.game.raiseEvent(
             'onAbilityInitiated',
