a17b566e0015abfeac718031e601564ed1080946
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-08-25 07:36:19 +0100

    timestopper (not complete)

---
+++ server/game/cards/Argaia/Timestopper.js
@@ -0,0 +1,48 @@
+const { BattlefieldTypes } = require('../../../constants.js');
+const Card = require('../../Card.js');
+
+class Timestopper extends Card {
+    setupCardAbilities(ability) {
+        this.entersPlay({
+            target: {
+                activePromptTitle: 'Choose a unit to prevent attack, block and guard',
+                optional: true,
+                cardType: BattlefieldTypes,
+                controller: 'opponent',
+                cardCondition: (card, context) => card !== context.source,
+                gameAction: ability.actions.cardLastingEffect((context) => ({
+                    effect: [
+                        ability.effects.cardCannot('attack'),
+                        ability.effects.cardCannot('guard'),
+                        ability.effects.cardCannot('block')
+                    ],
+                    until: {
+                        onTurnEnded: (event) => event.player === context.player.opponent,
+                        onRoundEnded: (event) => true
+                    }
+                }))
+            }
+        });
+
+        this.reaction({
+            isLimited: true,
+            when: {
+                onCardEntersPlay: (event, context) =>
+                    BattlefieldTypes.includes(event.card.type) &&
+                    event.card.controller === context.player.opponent
+            },
+            location: 'hand',
+            gameAction: ability.actions.playCard(() => ({
+                target: this,
+                ignoreActionCost: true,
+                isLimited: true,
+                playedAsReaction: true
+            })),
+            preferActionPromptMessage: true
+        });
+    }
+}
+
+Timestopper.id = 'timestopper';
+
+module.exports = Timestopper;
