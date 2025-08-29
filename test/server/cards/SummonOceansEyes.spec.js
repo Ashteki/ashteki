6dfbb282605fd99d3c7ff9e09a967458d9b2c5f0
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-08-29 18:32:40 +0100

    Ocean's Eyes

---
+++ test/server/cards/SummonOceansEyes.spec.js
@@ -0,0 +1,29 @@
+describe("Summon Ocean's Eyes", function () {
+    beforeEach(function () {
+        this.setupTest({
+            player1: {
+                phoenixborn: 'brennen-blackcloud',
+                archives: ['oceans-eyes', 'oceans-eyes', 'oceans-eyes'],
+                spellboard: ['summon-oceans-eyes'],
+                hand: ['power-through'],
+                dicepool: ['time', 'natural', 'natural', 'time'],
+                inPlay: []
+            },
+            player2: {
+                phoenixborn: 'coal-roarkwin',
+                inPlay: ['flute-mage', 'hammer-knight']
+            }
+        });
+    });
+
+    it('Summon action', function () {
+        this.player1.clickCard(this.summonOceansEyes);
+        this.player1.clickPrompt("Summon Ocean's Eyes");
+        this.player1.clickDie(1);
+        this.player1.clickDie(2);
+        this.player1.clickDone();
+
+        expect(this.player1.inPlay.length).toBe(2);
+        expect(this.player1.archives.length).toBe(1);
+    });
+});
