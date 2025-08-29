fa653946e936ea1ebcd30a593c9747f070e36a35
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-08-29 07:34:46 +0100

    rising tides

---
+++ server/game/cards/Argaia/RisingTides.js
@@ -0,0 +1,29 @@
+const Card = require('../../Card.js');
+
+class RisingTides extends Card {
+    setupCardAbilities(ability) {
+        this.play({
+            title: "Rising Tides",
+            effect: 'deal 1 damage to all units at the end of next turn this round',
+            gameAction: ability.actions.delayedEffect((context) => {
+                const currentRound = this.game.round;
+                return {
+                    when: {
+                        onTurnEnded: (event) =>
+                            event.player !== context.player && event.round === currentRound
+                    },
+                    gameAction: ability.actions.orderedAoE((context) => ({
+                        promptTitle: 'Rising Tides',
+                        target: context.game.unitsInPlay,
+                        gameAction: ability.actions.dealDamage({ showMessage: true }),
+                    }))
+                };
+            })
+
+        });
+    }
+}
+
+RisingTides.id = 'rising-tides';
+
+module.exports = RisingTides;
