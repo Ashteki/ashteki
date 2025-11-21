639bcd9a2c93e63986ec75ef4c665071a5a21cf1
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2025-11-21 22:47:49 +0000

    thunder suit

---
+++ server/game/cards/Ascendancy/ThunderSuit.js
@@ -0,0 +1,34 @@
+const { Magic } = require('../../../constants.js');
+const Card = require('../../Card.js');
+
+class ThunderSuit extends Card {
+    setupCardAbilities(ability) {
+        this.whileAttached({
+            effect: [
+                ability.effects.modifyAttack(2),
+                ability.effects.modifyLife(2),
+                ability.effects.modifyRecover(1)
+            ]
+        });
+
+        this.forcedReaction({
+            when: {
+                onCardAttached: (event, context) => event.card === context.source
+            },
+            target: {
+                activePromptTitle: 'Choose an exhausted Artifice die',
+                optional: true,
+                toSelect: 'die',
+                owner: 'self',
+                dieCondition: (die) => die.magic === Magic.Artifice && die.exhausted,
+                gameAction: ability.actions.resolveDieAbility((context) => ({
+                    targetCard: context.source.parent
+                }))
+            }
+        });
+    }
+}
+
+ThunderSuit.id = 'thunder-suit';
+
+module.exports = ThunderSuit;
