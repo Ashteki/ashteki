7cc823cb01f346a6a29e33b73e6bfffa64220544
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2026-02-14 18:05:44 +0000

    text search on cards page

--- client/pages/CardsPage.jsx
+++ client/pages/CardsPage.jsx
@@ -2,43 +2,54 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './CardsPage.scss';
 import CardGallery from '../Components/Profile/CardGallery';
-import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { Form, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
 const CardsPage = () => {
     const allCards = useSelector((state) => state.cards.cards);
     const [deckType, setDeckType] = useState('player');
+    const [searchText, setSearchText] = useState('');
     const cardList = Object.values(allCards).filter(
         (c) =>
             (c.deckType || 'player') === deckType &&
             !c.stub.includes('ultimate') &&
-            !c.stub.includes('behaviour')
-    ); //.sort((a, b) => a.type + a.stub > b.type + b.stub ? 1 : -1);
+            !c.stub.includes('behaviour') &&
+            (!searchText || c.name.toLowerCase().includes(searchText.toLowerCase()))
+    );
+
     return (
         <div className='full-height'>
             <div className='lobby-card'>
-                <div className='lobby-header'>All Cards</div>
                 <div className='cards-filter'>
-                    <ToggleButtonGroup name="radio" value={deckType}>
+                    <ToggleButtonGroup name='radio' value={deckType}>
                         <ToggleButton
                             key={'rad-0'}
                             id={`radio-0`}
-                            type="radio"
+                            type='radio'
                             value='player'
-                            onChange={(e) => setDeckType('player')}
-                            className='mini'
+                            onChange={() => setDeckType('player')}
+                            className='mini def'
                         >
-                            Player cards                    </ToggleButton>
+                            Player cards
+                        </ToggleButton>
                         <ToggleButton
                             key={'rad-1'}
                             id={`radio-1`}
-                            type="radio"
+                            type='radio'
                             value='chimera'
-                            onChange={(e) => setDeckType('chimera')}
-                            className='mini'
+                            onChange={() => setDeckType('chimera')}
+                            className='mini def'
                         >
                             Chimera Cards
                         </ToggleButton>
                     </ToggleButtonGroup>
+
+                    <Form.Control
+                        id='search'
+                        type='text'
+                        placeholder='Search...'
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
                 </div>
                 <div className='card-browser'>
                     <CardGallery cards={cardList} />
