520e8638d6698d5ccfcffa42fe91c97b9df5db41
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2026-05-28 17:45:18 +0100

    galewind hawk

---
+++ server/game/cards/Ascendancy/SummonGalewindHawk.js
@@ -0,0 +1,24 @@
+const { Level, Magic } = require('../../../constants.js');
+const Card = require('../../Card.js');
+const DiceCount = require('../../DiceCount.js');
+
+class SummonGalewindHawk extends Card {
+    setupCardAbilities(ability) {
+        this.summon('galewind-hawk', {
+            title: 'Summon Galewind Hawk',
+            cost: [
+                ability.costs.mainAction(),
+                ability.costs.exhaust(),
+                ability.costs.dice([
+                    new DiceCount(1, Level.Class, Magic.Astral),
+                    new DiceCount(1, Level.Class, Magic.Nature)
+                ])
+            ],
+            location: 'spellboard'
+        });
+    }
+}
+
+SummonGalewindHawk.id = 'summon-galewind-hawk';
+
+module.exports = SummonGalewindHawk;
