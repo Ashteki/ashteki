767b313b4a7e5f885a15ae828a48f2df1d6e2aa2
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2026-05-28 22:51:29 +0100

    tailwind

---
+++ server/game/cards/Ascendancy/Tailwind.js
@@ -0,0 +1,31 @@
+const { Level } = require('../../../constants.js');
+const Card = require('../../Card.js');
+const DiceCount = require('../../DiceCount.js');
+
+class Tailwind extends Card {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Tailwind',
+            cost: [ability.costs.sideAction(), ability.costs.exhaust()],
+            location: 'spellboard',
+            gameAction: ability.actions.conditional({
+                condition: (context) => context.player.phoenixborn.isAirborne,
+                trueGameAction: ability.actions.cardLastingEffect((context) => ({
+                    duration: 'untilEndOfTurn',
+                    effect: ability.effects.modifyAttack(1),
+                    target: context.player.unitsInPlay
+                }))
+            }),
+            then: {
+                may: 'spend one basic die to take an additional side action',
+                cost: ability.costs.dice([new DiceCount(1, Level.Basic)]),
+                gameAction: ability.actions.addSideAction({ amount: 1 })
+
+            }
+        });
+    }
+}
+
+Tailwind.id = 'tailwind';
+
+module.exports = Tailwind;
