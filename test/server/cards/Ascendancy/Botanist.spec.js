86b7420b6a94d526324cd30fd30be982885f2860
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2026-05-30 11:26:20 +0100

    botanist and nutured, moved ascendance tests to subfolder

---
+++ test/server/cards/Ascendancy/Botanist.spec.js
@@ -0,0 +1,31 @@
+describe('Botanist', function () {
+    describe('in play', function () {
+        beforeEach(function () {
+            this.setupTest({
+                player1: {
+                    phoenixborn: 'maeve-luminvale',
+                    inPlay: ['botanist', 'snapseed', 'stormchaser'],
+                    dicepool: ['natural', 'natural', 'artifice', 'astral'],
+                    spellboard: ['summon-storm-spirit', 'summon-galewind-hawk'],
+                    archives: ['nurtured'],
+                    hand: ['anchornaut', 'hurricane', 'searing-bolt', 'rayward-knight']
+                },
+                player2: {
+                    phoenixborn: 'aradel-summergaard',
+                    dicepool: ['divine', 'divine', 'natural', 'natural', 'time', 'time'],
+                    inPlay: ['beast-tamer', 'blue-jaguar', 'hammer-knight'],
+                    spellboard: ['chant-of-revenge']
+                }
+            });
+        });
+
+        it('attach nurtured alteration to unit', function () {
+            this.player1.useAbility(this.botanist);
+            this.player1.clickCard(this.snapseed);
+            expect(this.snapseed.upgrades.length).toBe(1);
+            expect(this.snapseed.life).toBe(2);
+            expect(this.nurtured.location).toBe('play area');
+            expect(this.nurtured.parent).toBe(this.snapseed);
+        });
+    });
+});
