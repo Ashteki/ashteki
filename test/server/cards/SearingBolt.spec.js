8af290cc0a59d7c2404f59fbe9393ec3c22ccc6d
Dan Johnson <32267086+danj3000@users.noreply.github.com>
2026-06-02 22:42:43 +0100

    deck rename and ff change

--- test/server/cards/SearingBolt.spec.js
+++ test/server/cards/SearingBolt.spec.js
@@ -1,3 +1,5 @@
+const Dice = require('../../../server/game/dice');
+
 describe('Searing Bolt', function () {
     describe('with one target', function () {
         beforeEach(function () {
@@ -29,6 +31,46 @@ describe('Searing Bolt', function () {
             this.player1.clickCard(this.hammerKnight);
 
             expect(this.hammerKnight.damage).toBe(2);
+
+            expect(this.player1).toHaveDefaultPrompt();
+        });
+    });
+
+    describe('vs dragonborn', function () {
+        beforeEach(function () {
+            this.setupTest({
+                mode: 'dragonborn',
+                player1: {
+                    phoenixborn: 'kanna-galeheart',
+                    inPlay: ['flute-mage', 'iron-worker'],
+                    dicepool: ['divine', 'divine', 'natural', 'natural', 'astral', 'astral'],
+                    spellboard: ['summon-storm-spirit', 'summon-galewind-hawk'],
+                    archives: ['the-awakened-state'],
+                    hand: ['hurricane', 'rayward-knight'],
+                    deck: ['anchornaut', 'hurricane', 'searing-bolt', 'rayward-knight']
+                },
+                player2: {
+                    dummy: true,
+                    phoenixborn: 'scatha-kalani',
+                    behaviour: 'scatha-behaviour',
+                    ultimate: 'scatha-ultimate',
+                    threatZone: ['sear', 'aegis-feathers'],
+                    dicepool: ['dragon', 'dragon', 'dragon', 'dragon', 'dragon']
+                }
+            });
+        });
+
+        it('deal 2 damage to highest life unit', function () {
+            spyOn(Dice, 'getRandomInt').and.returnValue(4);
+            this.player1.endTurn();
+            this.player1.clickOk();
+
+            this.player1.useAbility(this.kannaGaleheart);
+            this.player1.clickPrompt('Searing Bolt');
+            this.player1.play(this.searingBolt);
+            this.player1.clickCard(this.sear);
+
+            expect(this.sear.location).toBe('discard');
 
             expect(this.player1).toHaveDefaultPrompt();
         });
@@ -96,5 +138,4 @@ describe('Searing Bolt', function () {
             expect(this.player1).toHaveDefaultPrompt();
         });
     });
-
 });
